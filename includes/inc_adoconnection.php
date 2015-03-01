506c1907d7bea1acf3462281e9c2f01cc5393541
martti <info@martti.be>
2015-03-01 23:17:08 +0100

    getenv database

--- includes/inc_adoconnection.php
+++ includes/inc_adoconnection.php
@@ -4,10 +4,13 @@ require_once('adodb/adodb-errorpear.inc.php');
 require_once("adodb/adodb.inc.php");
 require_once($rootpath."includes/inc_config.php");
 
-$db_dsn=$xmlconfig->dbdriver ."://" . $xmlconfig->dbuser .":" .$xmlconfig->dbpass . "@" .$xmlconfig->dbhost . "/" .$xmlconfig->dbname;
+//$db_dsn=$xmlconfig->dbdriver ."://" . $xmlconfig->dbuser .":" .$xmlconfig->dbpass . "@" .$xmlconfig->dbhost . "/" .$xmlconfig->dbname;
 
-$parseddsn=parse_url($db_dsn);
-$db = NewADOConnection($db_dsn);
+//$parseddsn=parse_url($db_dsn);
+
+//$db = NewADOConnection($db_dsn);
+$db = NewADOConnection(getenv('DATABASE_URL'));
+
 $db->SetFetchMode(ADODB_FETCH_ASSOC);
 
 if(!empty($xmlconfig->dbdebug) && $xmlconfig->dbdebug == 1){
