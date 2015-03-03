13b75d5db9c767fc3139981e4c6d50b4a21860c4
martti <info@martti.be>
2015-03-03 08:06:56 +0100

    parse url for redistogo

--- includes/inc_default.php
+++ includes/inc_default.php
@@ -54,12 +54,14 @@ $redis_url = getenv('REDISTOGO_URL');
 if(!empty($redis_url)){
 	Predis\Autoloader::register();
 	try {
-		$redispars = parse_url($redis_url);
-	    $redis = new Predis\Client($redis_url);
+		$redis_con = parse_url($redis_url);
+		$redis_con['password'] = $redis_con['pass'];
+		$redis_con['scheme'] = 'tcp';
+	    $redis = new Predis\Client($redis_con);
 
 	}
 	catch (Exception $e) {
-	    echo "Couldn't connected to Redis";
+	    echo "Couldn't connected to Redis: ";
 	    echo $e->getMessage();
 	}
 }
