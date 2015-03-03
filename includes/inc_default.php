809f966c26bd3ea043db24fd3b04ae16a241e86c
martti <info@martti.be>
2015-03-03 09:11:49 +0100

    session name to env var

--- includes/inc_default.php
+++ includes/inc_default.php
@@ -22,9 +22,28 @@ if(!isset($rootpath)){
 
 require_once($rootpath."includes/inc_eventlog.php");
 
-//create a session name on domain stripped from chars (attention! domains must differ in alphanumeric chars)
-
-$session_name = preg_replace('/[^A-Za-z0-9]/', '', $baseurl);
+/*
+ * get session name from environment variable ELAS_DOMAIN_SESSION_<domain>
+ * dots in <domain> are replaced by double underscore __
+ * hyphens in <domain> are replaced by triple underscore ___
+ *
+ * example:
+ *
+ * to link e-example.com to a session set environment variable
+ * ELAS_DOMAIN_SESSION_E___EXAMPLE__COM = <session_name>
+ *
+ * + the session name has to be set to the color name of the database!
+ * + session name is prefix of the image files.
+ * + session name is prefix of keys in Redis.
+ *
+ */
+ 
+$session_name = str_replace(':', '', $baseurl);
+$session_name = str_replace('.', '__', $session_name);
+$session_name = str_replace('-', '___', $session_name);
+$session_name = strtoupper($session_name);
+$session_name = getenv('ELAS_DOMAIN_SESSION_' . $session_name);
+$session_name = ($session_name) ? $session_name : 'ELASDEFAULT';
 
 session_name($session_name);
 session_start();
