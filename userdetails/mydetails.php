792e224ec8f5bb25d2eab16bb2ddf3577d050423
martti <info@martti.be>
2015-03-17 09:34:35 +0100

    rm mailinglists

--- userdetails/mydetails.php
+++ userdetails/mydetails.php
@@ -4,7 +4,6 @@ $rootpath = "../";
 $role = 'user';
 require_once($rootpath."includes/inc_default.php");
 require_once($rootpath."includes/inc_adoconnection.php");
-// require_once($rootpath."includes/inc_mailinglists.php");
 
 if (!isset($s_id))
 {
@@ -25,8 +24,6 @@ echo "<script type='text/javascript' src='" .$rootpath ."js/mydetails.js'></scri
 $user = readuser($s_id);
 show_user($user);
 show_editlink();
-//show_sendform();
-// show_pwform();
 
 $url = "rendercontact.php";
 echo "<div id='contactdiv'></div>";
@@ -37,12 +34,6 @@ echo "<ul class='hormenu'>";
 echo "<li><a href='mydetails_cont_add.php'>Contact toevoegen</a></li>";
 echo "</ul>";
 echo "</td></tr></table>";
-
-//show_contactadd();
-
-//	show_subs();
-// show_subform();
-// show_unsubform();
 
 show_oids();
 show_oidform();
@@ -55,55 +46,6 @@ include($rootpath."includes/inc_footer.php");
 
 function show_changepwlink($s_id){
 	echo "<p>| <a href='mydetails_pw.php?id=" .$s_id. "'>Paswoord veranderen</a> |</p>";
-}
-
-function show_sendform() {
-	global $rootpath;
-	
-	if(readconfigfromdb("mailinglists_enabled") == 1) {
-		global $s_id;
-		$lists = get_my_open_mailinglists($s_id);
-
-		echo "<div id='mlformdiv' class='hidden'>";
-		echo "<form action='". $rootpath ."/resources/mailmessage/new' id='msgform' method='post'>";
-		echo "<table class='selectbox' cellspacing='0' cellpadding='0' border='0'>";
-
-		// Topic
-		echo "<tr><td valign='top' align='right'>Aan lijst</td>";
-		echo "<td valign='top'>";
-		echo "<select name='list'>\n";
-		foreach ($lists as $value){
-			echo "<option value='".$value["listname"]."' >" .$value["listname"] ."</option>\n";
-		}
-		echo "</select>";
-		echo "</td>";
-		echo "</tr>";
-
-		// Moderatormail
-		echo "<tr><td valign='top' align='right'>Onderwerp</td>";
-		echo "<td valign='top'>";
-		echo "<input  type='text' id='msgsubject' name='msgsubject' size='80'>";
-		echo "</td>";
-		echo "</tr>";
-
-		echo "<tr><td valign='top' align='right'>Body</td>";
-		echo "<td valign='top'>";
-		echo "<textarea class='ckeditor' id='msgbody' name='msgbody' cols='80' rows='15'></textarea>";
-		echo "</td>";
-		echo "</tr>";
-
-		echo "<tr><td colspan='2' align='right'>";
-		echo "<input type='submit' id='zend' value='Verzenden' name='zend'>";
-		echo "</td><td>&nbsp;</td></tr>";
-		echo "</table>";
-		echo "</form>";
-		echo "</div>";
-	} else {
-		echo "<div id='mlformdiv' class='hidden'>";
-		echo "Mails verzenden is uitgeschakeld in de instellingen van deze installatie";
-		echo "<form action='". $rootpath ."/resources/mailmessage/new' id='msgform' method='post'>";
-		echo "</form></div>";
-	}
 }
 
 function show_oidform() {
@@ -124,77 +66,6 @@ function show_oidform() {
 	echo "</div>";
 }
 
-function show_subform(){
-	global $s_id, $rootpath;
-
-	if(readconfigfromdb("mailinglists_enabled") == 1) {
-		$lists = get_availablelists($s_id);
-
-		echo "<div id='subformdiv' class='hidden'>";
-		echo "<table class='selectbox' cellspacing='0' cellpadding='0' border='0'>";
-		echo "<tr><td>";
-		echo "<form action='". $rootpath ."/resources/user/subscription/new' id='subform' method='post'>";
-		echo "<select>";
-		foreach($lists as $key => $value){
-			echo "<option value='" .$value['listname'] ."'>" .$value['listname'] ."</option>";
-		}
-		echo "</select>";
-		echo "<input type='submit' value='Abonneren'>";
-		echo "</form>";
-		echo "</td></tr>";
-		echo "</table>";
-		echo "</div>";
-	} else {
-		echo "<div id='subformdiv' class='hidden'>";
-		echo "<form action='". $rootpath ."/resources/user/subscription/new' id='subform' method='post'>";
-		echo "Mailinglists zijn uitgeschakeld in de instellingen van deze installatie";
-		echo "</form></div>";
-	}
-}
-
-function show_unsubform(){
-	global $s_id, $rootpath;
-	
-	$lists = get_my_open_mailinglists($s_id);
-
-	if(readconfigfromdb("mailinglists_enabled") == 1) {
-		echo "<div id='unsubformdiv' class='hidden'>";
-		echo "<table class='selectbox' cellspacing='0' cellpadding='0' border='0'>";
-		echo "<tr><td>";
-		echo "<form action='". $rootpath ."/resources/user/subscription/' id='unsubform' method='post'>";
-		echo "<select>";
-		foreach($lists as $key => $value){
-			echo "<option value='" .$value['listname'] ."'>" .$value['listname'] ."</option>";
-		}
-		echo "</select>";
-		echo "<input type='submit' value='Uitschrijven'>";
-		echo "</form>";
-		echo "</td></tr>";
-		echo "</table>";
-		echo "</div>";
-	} else {
-		echo "<div id='unsubformdiv' class='hidden'>";
-		echo "<form action='". $rootpath ."/resources/user/subscription/' id='unsubform' method='post'>";
-		echo "Mailinglists zijn uitgeschakeld in de instellingen van deze installatie";
-		echo "</form></div>";
-	}
-}
-
-function show_subs(){
-	global $rootpath;
-	
-	$url = "rendersubscriptions.php";
-	echo "<div id='subsdiv'></div>";
-	echo "<script type='text/javascript'>showsmallloader('subsdiv');loadsubs('$url');</script>";
-	echo "<table width='100%' border=0><tr><td>";
-	echo "<ul class='hormenu'>";
-
-	echo "<li><a id='showsubform' href='#'>Abonnement toevoegen</a></li>";
-	echo "<li><a id='showunsubform' href='#'>Abonnement opzeggen</a></li>";
-	echo "</ul>";
-	echo "</td></tr></table>";
-}
-
 function show_oids(){
 	global $rootpath;
 	
@@ -212,88 +83,6 @@ function get_type_contacts(){
 	global $db;
 	$query = "SELECT * FROM type_contact";
 	return $db->GetArray($query);
-}
-
-function show_contactadd(){
-	global $rootpath;
-	global $s_id;
-	echo "<div id='contactformdiv' class='hidden'>";
-	echo "<form action='". $rootpath ."/userdetails/postcontact.php' id='contactform' method='post'>";
-	echo "<input type='hidden' name='contactmode' value='new'>";
-	echo "<input type='hidden' name='id_user' value='" .$s_id ."'>";
-	echo "<input type='hidden' name='contactid' value=''>";
-	echo "<table class='selectbox' cellspacing='0' cellpadding='0' border='0'>\n\n";
-	echo "<tr>\n";
-	echo "<td valign='top' align='right'>Type</td>\n";
-	echo "<td>";
-	echo "<select name='id_type_contact'>\n";
-	$typecontactrow = get_type_contacts();
-	foreach($typecontactrow as $key => $value){
-		echo "<option value='".$value["id"]."'>".$value["name"]."</option>\n";
-	}
-	echo "</select>\n</td>\n";
-
-	echo "</tr>\n\n<tr>\n<td></td>\n<td>";
-	echo "</td>\n";
-	echo "</tr>\n\n";
-
-	echo "<tr>\n";
-	echo "<td valign='top' align='right'>Waarde</td>\n";
-	echo "<td>";
-	echo "<input type='text' name='value' size='80'>";
-	echo "</td>\n";
-	echo "</tr>\n\n<tr>\n<td></td>\n<td>";
-	echo "</td>\n";
-	echo "</tr>\n\n";
-
-	echo "<tr>\n";
-	echo "<td valign='top' align='right'>Commentaar</td>\n";
-	echo "<td>";
-	echo "<input type='text' name='comments' size='50' ";
-	echo "</td>\n";
-	echo "</tr>\n\n<tr>\n<td></td>\n<td>";
-	echo "</td>\n";
-	echo "</tr>\n\n";
-
-	echo "<tr>\n";
-	echo "<td valign='top' align='right'></td>\n";
-	echo "<td>";
-	echo "<input type='checkbox' name='flag_public' CHECKED";
-	echo " value='1' >Ja, dit contact mag zichtbaar zijn voor iedereen";
-
-	echo "</td>\n";
-	echo "</tr>\n\n<tr>\n<td></td>\n<td>";
-	echo "</td>\n";
-	echo "</tr>\n\n";
-
-	echo "<tr>\n<td colspan='2' align='right'><input type='submit' name='zend' value='Opslaan'>";
-	echo "</td>\n</tr>\n\n";
-	echo "</table></form></div>";
-}
-
-function show_pwform(){
-	global $s_id, $rootpath;
-	
-	echo "<div id='pwformdiv' class='hidden'>";
-	echo "<form action='". $rootpath ."/userdetails/postpassword.php' id='pwform' method='post'>";
-	echo "<table class='selectbox' cellspacing='0' cellpadding='0' border='0'>";
-	echo "<tr><td valign='top' align='right'>Paswoord</td>";
-	echo "<td valign='top'>";
-	echo "<input  type='text' id='pw1' name='pw1' size='30'>";
-	echo "</td>";
-	echo "</tr>";
-	echo "<tr><td valign='top' align='right'>Herhaal paswoord</td>";
-	echo "<td valign='top'>";
-	echo "<input  type='test' id='pw2' name='pw2' size='30'>";
-	echo "</td>";
-	echo "</tr>";
-	echo "<tr><td colspan='2' align='right'>";
-	echo "<input type='submit' id='zend' value='Passwoord wijzigen' name='zend'>";
-	echo "</td><td>&nbsp;</td></tr>";
-	echo "</table>";
-	echo "</form>";
-	echo "</div>";
-
 }
 
 function show_editlink(){
@@ -411,12 +200,4 @@ function show_contact(){
     echo "<li><a href='mydetails_cont_add.php'>Contact toevoegen</a></li>";
 	echo "</ul>";
 	echo "</td></tr></table>";
-}
-
-function get_lists(){
-	global $db;
-	$query = "SELECT * FROM lists WHERE auth = 'open'";
-	$lists = $db->Execute($query);
-	//var_dump($lists);
-	return $lists;
 }
