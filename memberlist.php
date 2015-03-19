a51f536dfd9dfa8cdfcf303defcbe5d099a90ee6
martti <info@martti.be>
2015-03-19 08:16:25 +0100

    repair searchcat

--- memberlist.php
+++ memberlist.php
@@ -11,8 +11,7 @@ include($rootpath."includes/inc_header.php");
 
 //echo "<script type='text/javascript' src='$rootpath/js/moomemberlist.js'></script>";
 
-$user_orderby = $_GET["user_orderby"];
-$prefix = $_POST["prefix"];
+$prefix = ($_POST["prefix"]) ?: 'ALL';
 $posted_list["prefix"] = $prefix;
 $searchname = $_POST["searchname"];
 $sort = $_POST["sort"];
@@ -31,13 +30,11 @@ echo "Groep:";
 echo "</td><td>\n";
 echo "<select name='prefix'>\n";
 
-echo "<option value='ALL'>ALLE</option>";
-$list_prefixes = get_prefixes();
-foreach ($list_prefixes as $key => $value){
-	echo "<option value='" .$value["prefix"] ."'>" .$value["shortname"] ."</option>";
-}
+$query = "SELECT prefix, shortname FROM letsgroups WHERE apimethod ='internal' AND prefix IS NOT NULL";
+$prefixes = $db->GetAssoc($query);
+$prefixes['ALL'] = 'ALLE';
+render_select_options($prefixes, $prefix);
 
-//render_select_options($prefixes, $value['prefix']);
 echo "</select>\n";
 echo "</td>\n";
 echo "</tr>";
@@ -54,12 +51,7 @@ echo $sort_options = array(
 	'fullname' => 'naam',
 	'postcode' => 'postcode',
 	'saldo' => 'saldo');
-foreach ($sort_options as $option => $lang)
-{
-	echo '<option value="' . $option . '"';
-	echo ($option == $sort) ? ' selected="selected"' : '';
-	echo '>' . $lang . '</option>';
-}
+render_select_options($sort_options, $sort);
 echo "</select>\n";
 echo "</td>\n";
 echo "</tr>";
@@ -73,8 +65,16 @@ echo "</form>";
 
 //rendermembers
 echo "<td align='right'>";
-show_printversion($prefix_filterby);
-show_csvversion($prefix_filterby,$rootpath);
+
+echo "<a href='print_memberlist.php?prefix_filterby=" .$prefix_filterby . "'>";
+echo "<img src='".$rootpath."gfx/print.gif' border='0'> ";
+echo "Printversie</a>";
+
+echo "<a href='csv_memberlist.php?prefix_filterby=" .$prefix_filterby;
+echo "' target='new'>";
+echo "<img src='".$rootpath."gfx/csv.jpg' border='0'> ";
+echo "CSV Export</a>";
+        
 echo "</td></tr></table>";
 
 $query = 'SELECT * FROM users u
@@ -131,7 +131,9 @@ foreach($userrows as $key => $value){
 	$rownumb=$rownumb+1;
 	if($rownumb % 2 == 1){
 		echo "<tr class='uneven_row'>\n";
-	}else{
+	}
+	else
+	{
 			echo "<tr class='even_row'>\n";
 	}
 
@@ -139,11 +141,15 @@ foreach($userrows as $key => $value){
 		echo "<td nowrap valign='top' bgcolor='#f475b6'><font color='white' ><strong>";
 		echo $value["letscode"];
 		echo "</strong></font>";
-	}elseif(check_timestamp($value["cdate"],$newuserdays) == 1){
+	}
+	else if((time() - ($newuserdays * 60 * 60 * 24)) < strtotime($value['cdate']))
+	{
 		echo "<td nowrap valign='top' bgcolor='#B9DC2E'><font color='white'><strong>";
 		echo $value["letscode"];
 					echo "</strong></font>";
-	}else{
+	}
+	else
+	{
 		echo "<td nowrap valign='top'>";
 		echo $value["letscode"];
 	}
@@ -175,201 +181,5 @@ foreach($userrows as $key => $value){
 
 }
 echo "</table></div>";
-
-
-
-//show_outputdiv($posted_list["prefix"]);
-
-
-
-////////////////////////////////////////////////////////////////////////////
-//////////////////////////////F U N C T I E S //////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-
-function show_legend(){
-echo "<div><br><br><table>";
-echo "<tr>";
-echo "<td bgcolor='#f475b6'><font color='white'><strong>Rood blokje:</strong></font></td><td>Uitstapper</td>";
-echo "</tr><tr>";
-echo "<td bgcolor='B9DC2E'><font color='white'><strong>Groen blokje:</strong></font></td><td> Instapper</td>";
-echo "</tr></table></div>";
-}
-
-function show_outputdiv($prefix){
-        echo "<div id='memberdiv'>";
-	echo "</div>";
-}
-
-function show_header($prefix_filterby,$rootpath) {
-        //echo "<form method='POST' action='memberlist.php'>";
-    echo "<table width='100%' border=0><tr><td>";
-	echo "<form method='POST' id='memberselect' action='" .$rootpath ."/rendermembers.php'>";
-
-        echo "<table  class='selectbox'>\n";
-
-        echo "<tr>\n<td>";
-        echo "Groep:";
-        echo "</td><td>\n";
-        echo "<select name='prefix'>\n";
-
-	echo "<option value='ALL'>ALLE</option>";
-	$list_prefixes = get_prefixes();
-	foreach ($list_prefixes as $key => $value){
-		echo "<option value='" .$value["prefix"] ."'>" .$value["shortname"] ."</option>";
-	}
-    echo "</select>\n";
-    echo "</td>\n";
-	echo "</tr>";
-
-	echo "<tr><td>Naam:</td><td>\n";
-    echo "<input type='text' name='searchname' size='25'>";
-	echo "</td>";
-    echo "</tr>";
-
-    echo "<tr>\n<td>Sorteer:</td><td>\n";
-    echo "<select name='sort'>\n";
-    echo "<option value='letscode'>letscode</option>";
-    echo "<option value='fullname'>naam</option>";
-    echo "<option value='postcode'>postcode</option>";
-    echo "</select>\n";
-    echo "</td>\n";
-	echo "</tr>";
-
-	echo "<tr><td align='right' colspan=2>";
-	echo "<input type='submit' name='zend' value='Weergeven'>";
-	echo "</td>";
-	echo "</tr>";
-	echo "</table>";
-	echo "</form>";
-
-	echo "<td align='right'>";
-	show_printversion($prefix_filterby);
-	show_csvversion($prefix_filterby,$rootpath);
-	echo "</td></tr></table>";
-
-}
-
-function get_contacts($userid){
-	global $db;
-	$query = "SELECT * FROM contact ";
-	$query .= " WHERE id_user =".$userid;
- 	$query .= " AND contact.flag_public = 1";
-	$contactrows = $db->GetArray($query);
-	return $contactrows;
-}
-
-function show_printversion($prefix_filterby){
-	echo "<a href='print_memberlist.php?prefix_filterby=" .$prefix_filterby . "'>";
-	echo "<img src='".$rootpath."gfx/print.gif' border='0'> ";
-	echo "Printversie</a>";
-}
-
-function show_csvversion($prefix_filterby,$rootpath){
-	echo "<a href='csv_memberlist.php?prefix_filterby=" .$prefix_filterby;
-        echo "' target='new'>";
-        echo "<img src='".$rootpath."gfx/csv.jpg' border='0'> ";
-        echo "CSV Export</a>";
-}
-
-function check_timestamp($cdate,$agelimit){
-        // agelimit is the time after which it expired
-        $now = time();
-	// age should be converted to seconds
-        $limit = $now - ($agelimit * 60 * 60 * 24);
-        $timestamp = strtotime($cdate);
-
-        if($limit < $timestamp) {
-                return 1;
-        } else {
-                return 0;
-        }
-}
-
-function show_all_users($userrows){
-	echo "<div class='border_b'><table class='data' cellpadding='0' cellspacing='0' border='1' width='99%'>\n";
-	echo "<tr class='header'>\n";
-	echo "<td><strong>";
-	echo "<a href='memberlist.php?user_orderby=letscode'>Code</a>";
-	echo "</strong></td>\n";
-	echo "<td><strong>";
-	echo "<a href='memberlist.php?user_orderby=fullname'>Naam</a>";
-	echo "</strong></td>\n";
-	echo "<td><strong>Tel</strong></td>\n";
-	echo "<td><strong>gsm</strong></td>\n";
-	echo "<td><strong>";
-	echo "<a href='memberlist.php?user_orderby=postcode'>Postc</a>";
-	echo "</strong></td>\n";
-	echo "<td><strong>Mail</strong></td>\n";
-	echo "<td><strong>Stand</strong></td>\n";
-	echo "</tr>\n\n";
-	$rownumb=0;
-	$newuserdays = readconfigfromdb("newuserdays");
-	foreach($userrows as $key => $value){
-	 	$rownumb=$rownumb+1;
-		if($rownumb % 2 == 1){
-			echo "<tr class='uneven_row'>\n";
-		}else{
-	        	echo "<tr class='even_row'>\n";
-		}
-
-		if($value["status"] == 2){
-			echo "<td nowrap valign='top' bgcolor='#f475b6'><font color='white' ><strong>";
-			echo $value["letscode"];
-			echo "</strong></font>";
-		}elseif(check_timestamp($value["cdate"],$newuserdays) == 1){
-			echo "<td nowrap valign='top' bgcolor='#B9DC2E'><font color='white'><strong>";
-			echo $value["letscode"];
-                        echo "</strong></font>";
-		}else{
-			echo "<td nowrap valign='top'>";
-			echo $value["letscode"];
-		}
-
-		echo"</td>\n";
-		echo "<td valign='top'>";
-		echo "<a href='memberlist_view.php?id=".$value["id"]."'>".htmlspecialchars($value["fullname"],ENT_QUOTES)."</a></td>\n";
-		echo "<td nowrap  valign='top'>";
-		$userid = $value["id"];
-		$contactrows = get_contacts($userid);
-
-			foreach($contactrows as $key2 => $value2){
-				if ($value2["id_type_contact"] == 1){
-					echo  $value2["value"];
-				break;
-				}
-			}
-		echo "</td>\n";
-		echo "<td nowrap valign='top'>";
-			foreach($contactrows as $key2 => $value2){
-				if ($value2["id_type_contact"] == 2){
-					echo $value2["value"];
-					break;
-				}
-			}
-		echo "</td>\n";
-		echo "<td nowrap valign='top'>".$value["postcode"]."</td>\n";
-		echo "<td nowrap valign='top'>";
-			foreach($contactrows as $key2 => $value2){
-				if ($value2["id_type_contact"] == 3){
-					echo "<a href='mailto:".$value2["value"]."'>".$value2["value"]."</a>";
-					break;
-				}
-			}
-		echo "</td>\n";
-
-		echo "<td nowrap valign='top' align='right'>";
-		$balance = $value["saldo"];
-		if($balance < $value["minlimit"] || ($value["maxlimit"] != NULL && $balance > $value["maxlimit"])){
-			echo "<font color='red'> $balance </font>";
-		}else{
-			echo $balance;
-		}
-
-		echo "</td>\n";
-		echo "</tr>\n\n";
-
-	}
-	echo "</table></div>";
-}
 
 include($rootpath."includes/inc_footer.php");
--- searchcat.php
+++ searchcat.php
@@ -10,57 +10,68 @@ if(!isset($s_id)){
 	exit;
 }
 
+$q = $_GET['q'];
+
 include($rootpath."includes/inc_header.php");
 echo "<h1>Vraag & Aanbod</h1>";
-show_searchform($q,$s_user_postcode);
-// show_outputdiv();
+
+echo "<form method='get' action='$rootpath/messages/search.php'>";
+echo "<input type='text' name='q' size='40' ";
+echo " value='". $q ."'";
+echo ">";
+echo "<input type='submit' name='zend' value='Zoeken'>";
+echo "<br><small><i>Een leeg zoekveld geeft ALLE V/A als resultaat terug</i></small>";
+echo "</form>";
+
+$cats = $db->GetArray('SELECT * FROM categories ORDER BY fullname');
+
+echo "<div class='border_b'>";
+echo "<table class='data' cellpadding='0' cellspacing='0' border='1' width='99%'>";
+echo "<tr class='header'>";
+echo "<td><strong>Categorie</strong></td>";
+echo '<td>Vraag</td>';
+echo '<td>Aanbod</td>';
+echo "</tr>";
+
+foreach($cats as $value){
+
+	$row_class =  ($value["id_parent"]) ? 'uneven_row' : 'even_row';
+
+	echo "<tr class='" . $row_class . "'>";
+	echo "<td valign='top'>";
+	echo ($value['id_parent']) ? '' : '<strong>';
+	echo "<a href='searchcat_viewcat.php?id=".$value["id"]."'>";
+	echo htmlspecialchars($value["fullname"],ENT_QUOTES);
+	echo "</a>";
+	echo ($value['id_parent']) ? '' : '</strong>';
+	echo "</td>";
+
+	echo '<td>' . (($v = $value['stat_msgs_wanted']) ? $v : '') . '</td>';
+	echo '<td>' . (($v = $value['stat_msgs_offers']) ? $v : '') . '</td>';
+	echo "</tr>";
+}
+echo "</table></div>";
+
 if($s_accountrole != 'guest'){
 	echo "<h1>Andere (interlets) groepen raadplegen</h1>";
-	show_interletsgroups();
+	echo "<table class='data' cellpadding='0' cellspacing='0' border='1'>";
+	$letsgroups = $db->Execute("SELECT * FROM letsgroups WHERE apimethod <> 'internal'");
+	foreach($letsgroups as $key => $value){
+		echo "<tr><td nowrap>";
+		echo "<a href='#' onclick=window.open('interlets/redirect.php?letsgroup=" .$value["id"] ."','interlets','location=no,menubar=no,scrollbars=yes')>" .$value["groupname"] ."</a>";
+		echo "</td></tr>";
+	}
+	echo "</table>";
 }
 include($rootpath."includes/inc_footer.php");
 
 ///////////////////
 
-function show_searchform($q,$s_user_postcode){
-	global $rootpath;
-	echo "<form method='get' action='$rootpath/messages/search.php'>";
-	echo "<input type='text' name='q' size='40' ";
-	if (!empty($q)){
-		echo " value=".$q;
-	}
-	echo ">";
-	echo "<input type='submit' name='zend' value='Zoeken'>";
-	echo "<br><small><i>Een leeg zoekveld geeft ALLE V/A als resultaat terug</i></small>";
-/*	if(!empty($s_user_postcode) && filter_var($s_user_postcode, FILTER_VALIDATE_INT)) {
-		echo "<br><small><i>Maximum afstand rond je gemeente : <input type='text' size='1' name='distance'> km.</i></small>";
-	} else {
-		echo "<br><small><i>Geef uw postcode in bij 'Mijn gegevens' om op max. afstand te kunnen zoeken</i></small>";
-	} */
-	echo "</form>";
-}
 
 function show_outputdiv(){
         echo "<div id='output'><img src='/gfx/ajax-loader.gif' ALT='loading'>";
         echo "<script type=\"text/javascript\">loadurl('rendersearchcat.php')</script>";
         echo "</div>";
-}
-
-function show_interletsgroups(){
-	global $db;
-	echo "<table class='data' cellpadding='0' cellspacing='0' border='1'>";
-	$query = "SELECT * FROM letsgroups WHERE apimethod <> 'internal'";
-	$letsgroups = $db->Execute($query);
-	foreach($letsgroups as $key => $value){
-		echo "<tr><td nowrap>";
-		//a href='#' onclick=window.open('$myurl','addgroup','width=640,height=480,scrollbars=yes,toolbar=no,location=no,menubar=no')>Groep toevoegen</a>
-		//echo "<a href='" .$value["url"] ."'>" .$value["groupname"] ."</a>";a
-		echo "<a href='#' onclick=window.open('interlets/redirect.php?letsgroup=" .$value["id"] ."','interlets','location=no,menubar=no,scrollbars=yes')>" .$value["groupname"] ."</a>";
-		echo "</td></tr>";
-	}
-
-	echo "</table>";
-
 }
 
 
