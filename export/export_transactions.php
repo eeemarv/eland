b01695448353142497887910bab7f894a39ab4a6
martti <info@martti.be>
2015-06-28 20:20:34 +0200

    fix #10 define rights for transactions export

--- export/export_transactions.php
+++ export/export_transactions.php
@@ -1,99 +1,55 @@
 <?php
 ob_start();
-$rootpath = "../";
-require_once($rootpath."includes/inc_default.php");
-require_once($rootpath."includes/inc_adoconnection.php");
+$rootpath = '../';
+$role = 'admin';
+require_once $rootpath . 'includes/inc_default.php';
+require_once $rootpath . 'includes/inc_adoconnection.php';
 
 $user_userid = $_GET["userid"];
 $user_datefrom = $_GET["datefrom"];
 $user_dateto = $_GET["dateto"];
 
-if(isset($s_id) && ($s_accountrole == "admin")){
-	show_ptitle($user_userid,$user_datefrom,$user_dateto);
-	$transactions = get_all_transactions();
-	show_all_transactions($transactions);
-}else{
-	redirect_login($rootpath);
-}
+$transactions = $db->GetArray('select 
+		t.transid, 
+		t.description,
+		fu.name AS from_name, tu.name AS to_name,
+		fu.letscode AS from_letscode, tu.letscode AS to_letscode,
+		t.cdate AS cdatum,
+		t.real_from,
+		t.real_to,
+		t.amount
+	from transactions t, users fu, users tu
+	where t.id_to = tu.id
+	and t.id_from = fu.id
+	order by t.date desc');
 
-/////////////////
+header("Content-disposition: attachment; filename=elas-transactions-".date("Y-m-d").".csv");
+header("Content-Type: application/force-download");
+header("Content-Transfer-Encoding: binary");
+header("Pragma: no-cache");
+header("Expires: 0");
 
-function redirect_login($rootpath){
-	header("Location: ".$rootpath."login.php");
-}
+echo '"Datum","Van", "interlets", "Aan", "interlets", "Bedrag","Dienst", "transactie id"';
+echo "\r\n";
 
-function show_ptitle(){
-        header("Content-disposition: attachment; filename=elas-transactions-".date("Y-m-d").".csv");
-        header("Content-Type: application/force-download");
-        header("Content-Transfer-Encoding: binary");
-        header("Pragma: no-cache");
-        header("Expires: 0");
-}
-
-function get_user($id){
-        global $db;
-        $query = "SELECT *";
-        $query .= " FROM users ";
-        $query .= " WHERE id='".$id."'";
-        $user = $db->GetRow($query);
-        return $user;
-}
-
-function get_users(){
-	global $db;
-        $query = "SELECT * FROM users ";
-        $query .= "WHERE (status = 1  ";
-        $query .= "OR status =2 OR status = 3)  ";
-        $query .= "AND users.accountrole <> 'guest' ";
-        $query .= " order by letscode";
-        $list_users = $db->GetArray($query);
-        return $list_users;
-}
-
-function show_all_transactions($transactions){
-
-        //echo '"id","Creatiedatum","Transactiedatum","Van","Aan","Bedrag","Dienst","Ingebracht door"';
-	echo '"Datum","Van","Aan","Bedrag","Dienst"';
-        echo "\r\n";
-	foreach($transactions as $key => $value){
-		echo "\"";
-                echo $value["datum"];
-		echo "\",";
-		echo "\"";
-                //echo htmlspecialchars($value["fromusername"],ENT_QUOTES). " (" .trim($value["fromletscode"]).")";
-		echo htmlspecialchars($value["fromletscode"],ENT_QUOTES);
-		echo "\",";
-		echo "\"";
-                //echo htmlspecialchars($value["tousername"],ENT_QUOTES). " (" .trim($value["toletscode"]).")";
-		echo htmlspecialchars($value["toletscode"],ENT_QUOTES);
-		echo "\",";
-		echo "\"";
-                echo $value["amount"];
-		echo "\",";
-		echo "\"";
-                echo htmlspecialchars($value["description"],ENT_QUOTES);
-		echo "\"";
-
-                echo "\r\n";
-        }
-}
-
-function get_all_transactions(){
-        global $db;
-
-        $query = "SELECT *, ";
-        $query .= " transactions.id AS transid, ";
-        $query .= " fromusers.id AS userid, ";
-        $query .= " fromusers.name AS fromusername, tousers.name AS tousername, ";
-        $query .= " fromusers.letscode AS fromletscode, tousers.letscode AS toletscode, ";
-        $query .= " transactions.date AS datum, ";
-        $query .= " transactions.cdate AS cdatum ";
-        $query .= " FROM transactions, users  AS fromusers, users AS tousers";
-	$query .= " WHERE transactions.id_to = tousers.id";
-        $query .= " AND transactions.id_from = fromusers.id";
-        $query .= " ORDER BY transactions.date DESC";
-	$query .= " LIMIT 5000";
-
-        $transactions = $db->GetArray($query);
-        return $transactions;
+foreach($transactions as $value)
+{
+	echo '"';
+	echo $value['cdatum'];
+	echo '","';
+	echo $value['from_letscode'] . ' ' . $value['from_name'];
+	echo '","';
+	echo $value['real_from'];
+	echo '","';
+	echo $value['to_letscode'] . ' ' . $value['to_name'];
+	echo '","';
+	echo $value['real_to'];
+	echo '","';
+	echo $value['amount'];
+	echo '","';
+	echo $value['description'];
+	echo '","';
+	echo $value['transid'];
+	echo '"';
+	echo "\r\n";
 }
