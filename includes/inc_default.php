2fdc22afb6ba76634a4256c81259968a4b920354
martti <info@martti.be>
2015-07-30 22:22:05 +0200

    rm obsolete files

--- includes/inc_default.php
+++ includes/inc_default.php
@@ -1,21 +1,10 @@
 <?php
 
-// Get rid of missing rootpath errors
-if(!isset($rootpath)){
-	$rootpath = "";
-}
-
-//override the include path, so we pick up the contrib directory first  //
-ini_set('include_path',$rootpath.'contrib/includes:'.ini_get('include_path'));
-
-//require_once($rootpath."includes/inc_config.php");
-
 $baseurl = $_SERVER['HTTP_HOST'];
 
-// Get rid of missing rootpath errors
 if(!isset($rootpath))
 {
-	$rootpath = "";
+	$rootpath = '';
 }
 
 $cdn_jqplot = (getenv('ELAS_CDN_JQPLOT')) ?: '//cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.8/';
