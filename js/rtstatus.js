81603d096576e83b9f23420deb9c3652124578e3
martti <info@martti.be>
2015-08-15 11:35:36 +0200

    rm obsolete js files

--- js/rtstatus.js
+++
@@ -1,8 +0,0 @@
-window.addEvent('domready', function() {
-    var evtSource = new EventSource("/realtimestatus.php");
-
-    evtSource.addEventListener("status", function(e) {
-        var obj = JSON.parse(e.data);
-        jsnotify(obj.message);
-    }, false);
-});
