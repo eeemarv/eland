52b679be9e2a89f36cff1b5619c856d7f4623779
martti <info@martti.be>
2015-11-01 20:52:31 +0100

    cache no store on logout

--- logout.php
+++ logout.php
@@ -6,6 +6,7 @@ require_once $rootpath . 'includes/inc_default.php';
 session_destroy();
 $cookie_params = session_get_cookie_params();
 setcookie(session_name(), '', 0, $cookie_params['path'], $cookie_params['domain'],
-	$cookie_params['secure'], $cookie_params['httponly']);
+	$cookie_params['secure'], $cookie_params['httponly']); 
 $_SESSION = array();
-redirect_login();
+header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+header('Location: ' . $rootpath . 'login.php');
