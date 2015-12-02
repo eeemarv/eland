dbee4d671fa6db24da5d2ef71abd8ab5b20da106
martti <info@martti.be>
2015-12-02 08:51:54 +0100

    session in redis

---
+++ includes/redis_session.php
@@ -0,0 +1,51 @@
+<?php
+
+class redis_session implements SessionHandlerInterface
+{
+	public $ttl = 86400; // 1 day
+	protected $redis;
+	protected $schema;
+
+	public function __construct(\Predis\Client $redis, $schema = 'PHPSESSID:')
+	{
+		$this->redis = $redis;
+		$this->schema = $schema;
+	}
+
+	public function open($save_path, $session_name)
+	{
+		// No action necessary because connection is injected
+		// in constructor and arguments are not applicable.
+	}
+
+	public function close()
+	{
+		$this->redis = null;
+		unset($this->redis);
+	}
+
+	public function read($id)
+	{
+		$id = $this->schema . '_' . $id;
+		$session_data = $this->redis->get($id);
+		$this->redis->expire($id, $this->ttl);
+		return $session_data;
+	}
+
+	public function write($id, $session_data)
+	{
+		$id = $this->schema . '_' . $id;
+		$this->redis->set($id, $session_data);
+		$this->redis->expire($id, $this->ttl);
+	}
+
+	public function destroy($id)
+	{
+		$this->redis->del($this->schema . '_' . $id);
+	}
+
+	public function gc($max_lifetime)
+	{
+		// no action necessary because using EXPIRE
+	}
+}
