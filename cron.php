f83aa93d0ebf5b09612385b716a9cfacd8eb7d04
martti <info@martti.be>
2015-12-07 13:38:35 +0100

    catch exception typeahead fetch cron

--- cron.php
+++ cron.php
@@ -170,74 +170,90 @@ if (!isset($schema_interletsq_min))
 		}
 
 		if (!$err)
-		{	
-			$client = new Goutte\Client();
-
-			$crawler = $client->request('GET', $letsgroup['url'] . '/login.php?token=' . $token);
-			$crawler = $client->request('GET', $letsgroup['url'] . '/rendermembers.php');
-
-			$users = array();
-
-			$crawler->filter('table > tr')->first()->nextAll()->each(function ($node) use (&$users)
+		{
+			try
 			{
-				$user = array();
+				$client = new Goutte\Client();
 
-				$td = $node->filter('td')->first();
-				$bgcolor = $td->attr('bgcolor');
-				$postcode = $td->siblings()->eq(3)->text();
+				$crawler = $client->request('GET', $letsgroup['url'] . '/login.php?token=' . $token);
+				$crawler = $client->request('GET', $letsgroup['url'] . '/rendermembers.php');
 
-				$user['c'] = $td->text();
-				$user['n'] = $td->nextAll()->text();
+				$users = array();
 
-				if ($bgcolor)
+				$crawler->filter('table > tr')
+					->first()
+					->nextAll()
+					->each(function ($node) use (&$users)
 				{
-					$user['s'] = (strtolower(substr($bgcolor, 1, 1)) > 'c') ? 2 : 3;
+					$user = array();
+
+					$td = $node->filter('td')->first();
+					$bgcolor = $td->attr('bgcolor');
+					$postcode = $td->siblings()->eq(3)->text();
+
+					$user['c'] = $td->text();
+					$user['n'] = $td->nextAll()->text();
+
+					if ($bgcolor)
+					{
+						$user['s'] = (strtolower(substr($bgcolor, 1, 1)) > 'c') ? 2 : 3;
+					}
+
+					if ($postcode)
+					{
+						$user['p'] = $postcode;
+					} 
+
+					$users[] = $user;
+				});
+
+				$redis_data_key = $letsgroup['url'] . '_typeahead_data';
+				$data_string = json_encode($users);
+				
+				if ($data_string != $redis->get($redis_data_key))
+				{
+					$redis_thumbprint_key = $letsgroup['url'] . '_typeahead_thumbprint';
+					$redis->set($redis_thumbprint_key, time());
+					$redis->expire($redis_thumbprint_key, 5184000);	// 60 days
+					$redis->set($redis_data_key, $data_string);
 				}
+				$redis->expire($redis_data_key, 86400);		// 1 day
 
-				if ($postcode)
-				{
-					$user['p'] = $postcode;
-				} 
+				$redis_refresh_key = $letsgroup['url'] . '_typeahead_updated';
+				$redis->set($redis_refresh_key, '1');
+				$redis->expire($redis_refresh_key, 43200);		// 12 hours
 
-				$users[] = $user;
-			});
+				$user_count = count($users);
 
-			$redis_data_key = $letsgroup['url'] . '_typeahead_data';
-			$data_string = json_encode($users);
-			
-			if ($data_string != $redis->get($redis_data_key))
-			{
-				$redis_thumbprint_key = $letsgroup['url'] . '_typeahead_thumbprint';
-				$redis->set($redis_thumbprint_key, time());
-				$redis->expire($redis_thumbprint_key, 5184000);	// 60 days
-				$redis->set($redis_data_key, $data_string);
+				$redis_user_count_key = $letsgroup['url'] . '_active_user_count';
+				$redis->set($redis_user_count_key, $user_count);
+				$redis->expire($redis_user_count_key, 86400); // 1 day
+
+				log_event('', 'Cron', 'typeahead data fetched of ' . $user_count . ' users from group ' . $letsgroup['groupname']);
+
+				echo '----------------------------------------------------' . $r;
+				echo $redis_data_key . $r;
+				echo $redis_refresh_key . $r;
+				echo 'user count: ' . $user_count . $r;
+				echo '----------------------------------------------------' . $r;
+				echo 'end Cron ' . "\n";
+
+				$redis->set($schema . '_cron_timestamp', time());
+
+				exit;
 			}
-			$redis->expire($redis_data_key, 86400);		// 1 day
+			catch (Exception $e)
+			{
+				$err = $e->getMessage();
+				echo $err . $r;
+				$redis_key = $schema . '_typeahead_failed_' . $letsgroup['remoteapikey'];
+				$redis->set($redis_key, '1');
+				$redis->expire($redis_key, 21600);  // 6 hours
 
-			$redis_refresh_key = $letsgroup['url'] . '_typeahead_updated';
-			$redis->set($redis_refresh_key, '1');
-			$redis->expire($redis_refresh_key, 43200);		// 12 hours
-
-			$user_count = count($users);
-
-			$redis_user_count_key = $letsgroup['url'] . '_active_user_count';
-			$redis->set($redis_user_count_key, $user_count);
-			$redis->expire($redis_user_count_key, 86400); // 1 day
-
-			log_event('', 'Cron', 'typeahead data fetched of ' . $user_count . ' users from group ' . $letsgroup['groupname']);
-
-			echo '----------------------------------------------------' . $r;
-			echo $redis_data_key . $r;
-			echo $redis_refresh_key . $r;
-			echo 'user count: ' . $user_count . $r;
-			echo '----------------------------------------------------' . $r;
-			echo 'end Cron ' . "\n";
-
-			$redis->set($schema . '_cron_timestamp', time());
-
-			exit;
+			}
 		}
-		else
+
+		if ($err)
 		{
 			echo '-- retry after 6 hours --' . $r;
 			echo '-- continue --' . $r;
