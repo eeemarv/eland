6b3b08d93cb2a5cfe5779401f2999518784a48c3
martti <info@martti.be>
2016-03-19 06:43:50 +0100

    rm cronjobs from elas logs

--- cron.php
+++ cron.php
@@ -897,10 +897,12 @@ function run_cronjob($name, $interval = 300, $enabled = null)
 		$db->insert('cron', array('cronjob' => $name, 'lastrun'	=> $now));
 	}
 
+/*
 	if ($name != 'cronschedule')
 	{
 		log_event(0, 'cron', 'Cronjob ' . $name . ' finished.');
 	}
+*/
 
 	echo '+++ Cronjob ' . $name . ' finished. +++' . $r;
 
