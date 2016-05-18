91544f012b5acdfb21ad46ecc9be424034c87444
martti <info@martti.be>
2016-05-18 10:01:02 +0200

    forum reorder and include form token

--- forum.php
+++ forum.php
@@ -8,9 +8,29 @@ $fa = 'comments-o';
 
 $mdb->connect();
 
-$topic = $_GET['t'];
-$del = ($_GET['del']) ?: false;
-$edit = ($_GET['edit']) ?: false;
+$topic = (isset($_GET['t'])) ? $_GET['t'] : false;
+$del = (isset($_GET['del'])) ? $_GET['del'] : false;
+$edit = (isset($_GET['edit'])) ? $_GET['edit'] : false;
+$add = (isset($_GET['add'])) ? true : false;
+
+if (!($s_user || $s_admin))
+{
+	if ($del)
+	{
+		$alert->error('Je hebt geen rechten om te verwijderen.');
+		cancel();
+	}
+	if ($add)
+	{
+		$alert->error('Je hebt geen rechten om te toe te voegen.');
+		cancel();
+	}
+	if ($edit)
+	{
+		$alert->error('Je hebt geen rechten om aan te passen.');
+		cancel();
+	}
+}
 
 $submit = ($_POST['zend']) ? true : false;
 
@@ -106,7 +126,6 @@ if ($submit)
 	{
 		$forum_post['ts'] = gmdate('Y-m-d H:i:s');
 		$forum_post['uid'] = $s_id;
-//		$forum_post['edit_count'] = 0;
 	}
 
     $errors = array();
@@ -179,8 +198,11 @@ if ($del)
 	exit;
 }
 
-if (!$edit)
+if ($add || $edit)
 {
+	$includejs = '<script src="' . $cdn_ckeditor . '"></script>
+		<script src="' . $rootpath . 'js/forum.js"></script>';
+
 	if ($topic)
 	{
 		$topic_id = new MongoId($topic);
@@ -191,182 +213,18 @@ if (!$edit)
 			$alert->error('Je hebt geen toegang tot dit forum onderwerp.');
 			cancel();
 		}
+	}
 
-		$find = array('$or'=> array(array('parent_id' => $topic), array('_id' => $topic_id)));
+	if ($edit)
+	{
+		$h1 = ($topic) ? 'Reactie aanpassen' : 'Forum onderwerp aanpassen';
 	}
 	else
 	{
-		$find = array('parent_id' => array('$exists' => false));
+		$h1 = ($topic) ? 'Nieuwe reactie' : 'Nieuw forum onderwerp';
 	}
 
-	$forum_posts = $mdb->forum->find($find);
-	$forum_posts->sort(array('ts' => (($topic) ? 1 : -1)));
-
-	$forum_posts = iterator_to_array($forum_posts);
-
-	$s_owner = ($s_id && $forum_posts[$topic]['uid'] == $s_id) ? true : false;
-
-	if ($s_admin || $s_user)
-	{
-		$str = ($topic) ? 'Reactie' : 'Onderwerp';
-
-		$top_buttons .= '<a href="#add" class="btn btn-success"';
-		$top_buttons .= ' title="' . $str . ' toevoegen"><i class="fa fa-plus"></i>';
-		$top_buttons .= '<span class="hidden-xs hidden-sm"> ' . $str . ' Toevoegen</span></a>';
-	}
-
-	if (($s_admin || $s_owner) && $topic)
-	{
-		$top_buttons .= aphp('forum', 'del=' . $topic, 'Onderwerp verwijderen', 'btn btn-danger', 'Onderwerp verwijderen', 'times', true);
-	}
-
-	if ($topic)
-	{
-		$top_buttons .= aphp('forum', '', 'Forum onderwerpen', 'btn btn-default', 'Forum onderwerpen', 'comments', true);
-	}
-
-	$h1 = ($topic) ? $forum_posts[$topic]['subject'] : 'Forum';
-}
-else
-{
-	$h1 = ($topic) ? 'Reactie' : 'Onderwerp';
-	$h1 .= ' aanpassen';
-}
-
-$includejs = '<script src="' . $cdn_ckeditor . '"></script>
-	<script src="' . $rootpath . 'js/forum.js"></script>';
-
-require_once $rootpath . 'includes/inc_header.php';
-
-if (!$edit)
-{
-	if ($topic)
-	{
-		foreach ($forum_posts as $p)
-		{
-			$s_owner = (($p['uid'] == $s_id) && $s_id) ? true : false;
-
-			echo '<div class="panel panel-default printview">';
-
-			echo '<div class="panel-body">';
-			echo $p['content'];
-			link_user($forum_post['uid']);
-			echo '</div>';
-
-			echo '<div class="panel-footer">';
-			echo '<p>' . link_user((int) $p['uid']) . ' @' . $p['ts'];
-			echo ($p['edit_count']) ? ' Aangepast: ' . $p['edit_count'] : '';
-
-			if ($s_admin || $s_owner)
-			{
-				echo '<span class="inline-buttons pull-right">';
-				echo aphp('forum', 'edit=' . $p['_id'], 'Aanpassen', 'btn btn-primary btn-xs', false, 'pencil');
-				echo aphp('forum', 'del=' . $p['_id'], 'Verwijderen', 'btn btn-danger btn-xs', false, 'times');
-				echo '</span>';
-			}
-			echo '</p>';
-			echo '</div>';
-
-			echo '</div>';
-
-		}
-	}
-	else
-	{
-		echo '<div class="panel panel-info">';
-		echo '<div class="panel-heading">';
-
-		echo '<form method="get">';
-		echo '<div class="row">';
-		echo '<div class="col-xs-12">';
-		echo '<div class="input-group">';
-		echo '<span class="input-group-addon">';
-		echo '<i class="fa fa-search"></i>';
-		echo '</span>';
-		echo '<input type="text" class="form-control" id="q" name="q" value="' . $q . '" ';
-		echo 'placeholder="Zoeken">';
-		echo '</div>';
-		echo '</div>';
-		echo '</div>';
-		echo '</form>';
-
-		echo '</div>';
-		echo '</div>';
-
-		echo '<div class="panel panel-default printview">';
-
-		echo '<div class="table-responsive">';
-		echo '<table class="table table-bordered table-striped table-hover footable"';
-		echo ' data-filter="#q" data-filter-minimum="1">';
-		echo '<thead>';
-
-		echo '<tr>';
-		echo '<th>Onderwerp</th>';
-		echo '<th data-hide="phone, tablet">Gebruiker</th>';
-		echo '<th data-hide="phone, tablet" data-sort-initial="descending" ';
-		echo 'data-type="numeric">Tijdstip</th>';
-		echo ($s_guest) ? '' : '<th data-hide="phone, tablet">Zichtbaarheid</th>';
-		echo ($s_admin) ? '<th data-hide="phone,tablet">Verwijderen</th>' : '';
-		echo '</tr>';
-
-		echo '</thead>';
-		echo '<tbody>';
-
-		foreach($forum_posts as $p)
-		{
-			if ($p['access'] < $access_level)
-			{
-				continue;
-			}
-
-			$s_owner = ($s_id == $p['uid']) ? true : false;
-
-			echo '<tr>';
-
-			echo '<td>';
-			echo aphp('forum', 't=' . $p['_id'], $p['subject']);
-			echo '</td>';
-			echo '<td>' . link_user($p['uid']) . '</td>';
-			echo '<td data-value="' . strtotime($p['ts']) . '">' . $p['ts'] . '</td>';
-
-			if (!$s_guest)
-			{
-				$access = $acc_ary[$p['access']];
-				echo '<td><span class="label label-' . $access[1] . '">' . $access[0] . '</span></td>';
-			}
-
-			if ($s_admin)
-			{
-				echo '<td>';
-				echo aphp('forum', 'del=' . $p['_id'], 'Verwijderen', 'btn btn-danger btn-xs', false, 'times');
-				echo '</td>';
-			}
-			echo '</tr>';
-
-		}
-		echo '</tbody>';
-		echo '</table>';
-		echo '</div>';
-		echo '</div>';
-
-	}
-}
-
-if (!$s_guest)
-{
-	$hh = 'Reactie' . (($edit) ? ' aanpassen' : '');
-
-	if (!$edit)
-	{
-		if ($topic)
-		{
-			echo '<h3>Reactie</h3>';
-		}
-		else
-		{
-			echo '<h3>Nieuw onderwerp</h3>';
-		}
-	}
+	include $rootpath . 'includes/inc_header.php';
 
 	echo '<div class="panel panel-info" id="add">';
 	echo '<div class="panel-heading">';
@@ -419,14 +277,220 @@ if (!$s_guest)
 	$str = ($topic) ? 'Reactie' : 'Onderwerp';
 	$btn = ($edit) ? 'primary' : 'success';
 	$action = ($edit) ? 'aanpassen' : 'toevoegen';
+	$cancel_dest = ($topic) ? 't=' . $topic : '';
 
+	echo aphp('forum', $cancel_dest, 'Annuleren', 'btn btn-default') . '&nbsp;';
 	echo '<input type="submit" name="zend" value="' . $str . ' ' . $action . '" class="btn btn-' . $btn . '">';
+	generate_form_token();
 
 	echo '</form>';
 
 	echo '</div>';
 	echo '</div>';
+
+	include $rootpath . 'includes/inc_footer.php';
+	exit;
 }
+
+/**
+ * Show topic
+ */
+ 
+if ($topic)
+{
+	$topic_id = new MongoId($topic);
+	$topic_post = $mdb->forum->findOne(array('_id' => $topic_id));
+
+	if ($topic_post['access'] < $access_level)
+	{
+		$alert->error('Je hebt geen toegang tot dit forum onderwerp.');
+		cancel();
+	}
+
+	$find = array('$or'=> array(array('parent_id' => $topic), array('_id' => $topic_id)));
+
+	$forum_posts = $mdb->forum->find($find);
+	$forum_posts->sort(array('ts' => (($topic) ? 1 : -1)));
+
+	$forum_posts = iterator_to_array($forum_posts);
+
+	$s_owner = ($s_id && $forum_posts[$topic]['uid'] == $s_id) ? true : false;
+
+	if ($s_admin || $s_owner)
+	{
+		$top_buttons .= aphp('forum', 'del=' . $topic, 'Onderwerp verwijderen', 'btn btn-danger', 'Onderwerp verwijderen', 'times', true);
+	}
+
+	$top_buttons .= aphp('forum', '', 'Forum onderwerpen', 'btn btn-default', 'Forum onderwerpen', 'comments', true);
+
+	$includejs = '<script src="' . $cdn_ckeditor . '"></script>
+		<script src="' . $rootpath . 'js/forum.js"></script>';
+
+	$h1 = $forum_posts[$topic]['subject'];
+
+	require_once $rootpath . 'includes/inc_header.php';
+
+	foreach ($forum_posts as $p)
+	{
+		$s_owner = (($p['uid'] == $s_id) && $s_id) ? true : false;
+
+		echo '<div class="panel panel-default printview">';
+
+		echo '<div class="panel-body">';
+		echo $p['content'];
+		link_user($forum_post['uid']);
+		echo '</div>';
+
+		echo '<div class="panel-footer">';
+		echo '<p>' . link_user((int) $p['uid']) . ' @' . $p['ts'];
+		echo ($p['edit_count']) ? ' Aangepast: ' . $p['edit_count'] : '';
+
+		if ($s_admin || $s_owner)
+		{
+			echo '<span class="inline-buttons pull-right">';
+			echo aphp('forum', 'edit=' . $p['_id'], 'Aanpassen', 'btn btn-primary btn-xs', false, 'pencil');
+			echo aphp('forum', 'del=' . $p['_id'], 'Verwijderen', 'btn btn-danger btn-xs', false, 'times');
+			echo '</span>';
+		}
+		echo '</p>';
+		echo '</div>';
+
+		echo '</div>';
+	}
+
+	if ($s_user || $s_admin)
+	{
+		echo '<h3>Reactie toevoegen</h3>';
+
+		echo '<div class="panel panel-info" id="add">';
+		echo '<div class="panel-heading">';
+
+		echo '<form method="post" class="form-horizontal">';
+
+		echo '<div class="form-group">';
+		echo '<div class="col-sm-12">';
+		echo '<textarea name="content" class="form-control" id="content" rows="4" required>';
+		echo $forum_post['content'];
+		echo '</textarea>';
+		echo '</div>';
+		echo '</div>';
+
+		$str = ($topic) ? 'Reactie' : 'Onderwerp';
+		$btn = ($edit) ? 'primary' : 'success';
+		$action = ($edit) ? 'aanpassen' : 'toevoegen';
+
+		echo '<input type="submit" name="zend" value="Reactie toevoegen" class="btn btn-success">';
+		generate_form_token();
+
+		echo '</form>';
+
+		echo '</div>';
+		echo '</div>';
+	}
+
+	include $rootpath . 'includes/inc_footer.php';
+	exit;
+}
+
+/*
+ * show topic list
+ */
+
+$find = array('parent_id' => array('$exists' => false));
+
+$forum_posts = $mdb->forum->find($find);
+$forum_posts->sort(array('ts' => (($topic) ? 1 : -1)));
+
+$forum_posts = iterator_to_array($forum_posts);
+
+$s_owner = ($s_id && $forum_posts[$topic]['uid'] == $s_id) ? true : false;
+
+if ($s_admin || $s_user)
+{
+	$top_buttons .= aphp('forum', 'add=1', 'Onderwerp Toevoegen', 'btn btn-success', 'Onderwerp toevoegen', 'plus', true);
+}
+
+$h1 = 'Forum';
+
+require_once $rootpath . 'includes/inc_header.php';
+
+echo '<div class="panel panel-info">';
+echo '<div class="panel-heading">';
+
+echo '<form method="get">';
+echo '<div class="row">';
+echo '<div class="col-xs-12">';
+echo '<div class="input-group">';
+echo '<span class="input-group-addon">';
+echo '<i class="fa fa-search"></i>';
+echo '</span>';
+echo '<input type="text" class="form-control" id="q" name="q" value="' . $q . '" ';
+echo 'placeholder="Zoeken">';
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '</form>';
+
+echo '</div>';
+echo '</div>';
+
+echo '<div class="panel panel-default printview">';
+
+echo '<div class="table-responsive">';
+echo '<table class="table table-bordered table-striped table-hover footable"';
+echo ' data-filter="#q" data-filter-minimum="1">';
+echo '<thead>';
+
+echo '<tr>';
+echo '<th>Onderwerp</th>';
+echo '<th data-hide="phone, tablet">Gebruiker</th>';
+echo '<th data-hide="phone, tablet" data-sort-initial="descending" ';
+echo 'data-type="numeric">Tijdstip</th>';
+echo ($s_guest) ? '' : '<th data-hide="phone, tablet">Zichtbaarheid</th>';
+echo ($s_admin) ? '<th data-hide="phone,tablet">Acties</th>' : '';
+echo '</tr>';
+
+echo '</thead>';
+echo '<tbody>';
+
+foreach($forum_posts as $p)
+{
+	if ($p['access'] < $access_level)
+	{
+		continue;
+	}
+
+	$s_owner = ($s_id == $p['uid']) ? true : false;
+
+	echo '<tr>';
+
+	echo '<td>';
+	echo aphp('forum', 't=' . $p['_id'], $p['subject']);
+	echo '</td>';
+	echo '<td>' . link_user($p['uid']) . '</td>';
+	echo '<td data-value="' . strtotime($p['ts']) . '">' . $p['ts'] . '</td>';
+
+	if (!$s_guest)
+	{
+		$access = $acc_ary[$p['access']];
+		echo '<td><span class="label label-' . $access[1] . '">' . $access[0] . '</span></td>';
+	}
+
+	if ($s_admin)
+	{
+		echo '<td>';
+		echo aphp('forum', 'edit=' . $p['_id'], 'Aanpassen', 'btn btn-primary btn-xs', false, 'pencil');
+		echo '&nbsp;';
+		echo aphp('forum', 'del=' . $p['_id'], 'Verwijderen', 'btn btn-danger btn-xs', false, 'times');
+		echo '</td>';
+	}
+	echo '</tr>';
+
+}
+echo '</tbody>';
+echo '</table>';
+echo '</div>';
+echo '</div>';
 
 include $rootpath . 'includes/inc_footer.php';
 
