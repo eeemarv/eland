70727e3e716a5ef16868059f9bb469a0aba4dc20
martti <info@martti.be>
2016-06-21 10:31:00 +0200

    unset session on logout

--- logout.php
+++ logout.php
@@ -8,6 +8,7 @@ $cookie_params = session_get_cookie_params();
 setcookie(session_name(), '', 0, $cookie_params['path'], $cookie_params['domain'],
 	$cookie_params['secure'], $cookie_params['httponly']);
 
+unset($_SESSION);
 $_SESSION = [];
 
 header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
