10a8ef303887cb16e2c4c6428c7d674cd34630d9
martti <info@martti.be>
2016-06-21 11:39:10 +0200

    set cookie expire in the past on logout

--- logout.php
+++ logout.php
@@ -6,7 +6,7 @@ require_once $rootpath . 'includes/inc_default.php';
 $_SESSION = [];
 
 $cookie_params = session_get_cookie_params();
-setcookie(session_name(), '', 0, $cookie_params['path'], $cookie_params['domain'],
+setcookie(session_name(), '', time() - 86400, $cookie_params['path'], $cookie_params['domain'],
 	$cookie_params['secure'], $cookie_params['httponly']);
 
 session_destroy();
