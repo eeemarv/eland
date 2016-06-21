eccf234ea34df9b30c20e5cc975a1a193236a29f
martti <info@martti.be>
2016-06-21 11:25:45 +0200

    fix order session destroy

--- logout.php
+++ logout.php
@@ -3,13 +3,13 @@ $rootpath = '';
 $page_access = 'guest';
 require_once $rootpath . 'includes/inc_default.php';
 
-session_destroy();
+$_SESSION = [];
+
 $cookie_params = session_get_cookie_params();
 setcookie(session_name(), '', 0, $cookie_params['path'], $cookie_params['domain'],
 	$cookie_params['secure'], $cookie_params['httponly']);
 
-unset($_SESSION);
-$_SESSION = [];
+session_destroy();
 
 header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
 header('Location: ' . $rootpath . 'login.php');
