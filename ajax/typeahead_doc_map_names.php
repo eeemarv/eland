17e75f8015561e19de1af4168ee70b8eddf35317
martti <info@martti.be>
2016-07-10 20:27:43 +0200

    fetch from eland_extra typeahead_doc_map_names

--- ajax/typeahead_doc_map_names.php
+++ ajax/typeahead_doc_map_names.php
@@ -5,34 +5,26 @@ require_once $rootpath . 'includes/inc_default.php';
 
 $map_names = [];
 
-$rows = $exdb->get_many(['agg_schema' => $schema,
-	'agg_type' => 'doc',
-	'data->>\'map_name\'' => ['<>' => '']]);
+$st = $db->prepare('select distinct data->>\'map_name\' as map_name
+	from eland_extra.aggs
+	where agg_type = \'doc\'
+		and agg_schema = ?
+		and data->>\'map_name\' <> \'\'
+	order by data->>\'map_name\' asc');
 
-if (count($rows))
+$st->bindValue(1, $schema);
+
+$st->execute();
+
+while ($row = $st->fetch())
 {
-	foreach ($rows as $row)
-	{
-		$map_names[] = $row['data']['map_name'];
-	}
-}
-else
-{
-	$mdb->connect();
-
-	$cursor = $mdb->docs->find(['map_name' => ['$exists' => true]]);
-
-	foreach ($cursor as $c)
-	{
-		set_exdb('doc', $c);
-		$map_names[] = $c['map_name'];
-	}
+	$map_names[] = $row['map_name'];
 }
 
-$output = json_encode($map_names);
+$map_names = json_encode($map_names);
 
-invalidate_typeahead_thumbprint('doc_map_names', false, crc32($output));
+invalidate_typeahead_thumbprint('doc_map_names', false, crc32($map_names));
 
 header('Content-type: application/json');
 
-echo $output;
+echo $map_names;
