0cac3d9ecd01bedc21836c257e7c7d35ae058ec8
martti <info@martti.be>
2016-07-21 10:11:40 +0200

    cleanup config.js

--- js/config.js
+++ js/config.js
@@ -1,49 +1,11 @@
-
-
 $(document).ready(function(){
 
-	$('div.rich-edit').each(function(){
+	$('div.rich-edit').summernote({
 
-		$(this).summernote({
-			lang: 'nl-NL'
-		});
-
-/*
-		var name = $(this).prop('name');
-
-		CKEDITOR.replace($(this), {
-			language: 'nl',
-			customConfig: '',
-			toolbarGroups: [
-				{"name":"basicstyles","groups":["basicstyles"]},
-				{"name":"links","groups":["links"]},
-				{"name":"paragraph","groups":["list","blocks"]},
-				{"name":"document","groups":["mode"]}
-			],
-			removeButtons: 'Anchor,Subscript,Superscript,Styles,Specialchar,Table'
-		});
-
-*/
+		lang: 'nl-NL'
 
 	});
+
 });
 
 
-/*
-
-$(document).ready(function(){
-
-		CKEDITOR.replace('registration_top_text', {
-			language: 'nl',
-			customConfig: '',
-			toolbarGroups: [
-				{"name":"basicstyles","groups":["basicstyles"]},
-				{"name":"links","groups":["links"]},
-				{"name":"paragraph","groups":["list","blocks"]},
-				{"name":"document","groups":["mode"]}
-			],
-			removeButtons: 'Anchor,Subscript,Superscript,Styles,Specialchar,Table'
-		});
-});
-
-*/
