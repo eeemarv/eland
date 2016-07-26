d37aa397f8a814d78280847a6ce0d93439ccb856
martti <info@martti.be>
2016-07-26 08:28:42 +0200

    clone email inputs

--- js/config.js
+++ js/config.js
@@ -14,14 +14,6 @@ $(document).ready(function(){
 
 		var $add_input = $(this).siblings('div.add-input');
 
-		if (value_ary.length < max_inputs){
-
-			$add_input.removeClass('hidden');
-			$add_input.find('span.btn').click(function(){
-				
-			});
-		}
-
 		var $input = $form_group.find('input');
 
 		if (value_ary[0] && value_ary.length > 1){
@@ -31,20 +23,53 @@ $(document).ready(function(){
 
 		}
 
-		var $cloned_group = $form_group.clone();
+		if (value_ary.length < max_inputs){
 
-		$cloned_group.find('label').remove();
-		$cloned_group.find('input').attr('value','');
-		$cloned_group.find('div').addClass('col-sm-offset-3');
+			$add_input.removeClass('hidden');
+
+			$add_input.find('span.btn').click(function(){
+
+				var $cloned_group = $form_group.clone();
+
+				$cloned_group.find('label').remove();
+				$cloned_group.find('input').attr('value','');
+				$cloned_group.find('div').addClass('col-sm-offset-3');
+
+				$cloned_input = $cloned_group.find('input');
+
+				var inputs_num = $li.find('input').length;
+				inputs_num--;
+
+				$cloned_input.prop('name', name + '_' + inputs_num);
+				$cloned_input.prop('id', name + '_' + inputs_num);
+				$cloned_input.val('');
+
+				$cloned_group.insertBefore($add_input);
+
+				if ((inputs_num + 2) > max_inputs){
+
+					$add_input.addClass('hidden');
+
+				}
+			});
+		}
 
 		for (var i = 1; i < value_ary.length; i++){
 
-			$cloned_group.find('input').prop('name', name + '_' + i);
-			$cloned_group.find('input').prop('id', name + '_' + i);
+			$cloned_group = $form_group.clone();
+
+			$cloned_group.find('label').remove();
+			$cloned_group.find('input').attr('value','');
+			$cloned_group.find('div').addClass('col-sm-offset-3');
+
+			$cloned_input = $cloned_group.find('input');
+
+			$cloned_input.prop('name', name + '_' + i);
+			$cloned_input.prop('id', name + '_' + i);
 
 			if (value_ary[i]){
 			
-				$cloned_group.find('input').val(value_ary[i].trim());
+				$cloned_input.val(value_ary[i].trim());
 			}
 
 			$cloned_group.insertBefore($add_input);
