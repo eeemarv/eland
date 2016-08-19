825dd3965366a7bc640bfcbcf7993dfa82776a71
martti <info@martti.be>
2016-08-19 09:29:35 +0200

    inject monolog in mail task

--- includes/task/mail.php
+++ includes/task/mail.php
@@ -37,7 +37,90 @@ class mail
 	 */
 	public function run(array $data, bool $check_schema = false)
 	{
+		if ($check_schema)
+		{
+			if (!isset($data['schema']))
+			{
+				$app->monolog->error('mail error: mail in queue without schema');
+				return;
+			}
 
+			$schema = $data['schema'];
+			unset($data['schema']);
+
+			if (!readconfigfromdb('mailenabled', $schema))
+			{
+				$m = 'Mail functions are not enabled. ' . "\n";
+				echo $m;
+				$this->monolog->error('mail: ' . $m);
+				return ;
+			}
+		}
+
+		if (!isset($data['subject']))
+		{
+			$this->monolog->error('mail error: mail without subject', ['schema' => $schema]);
+			return;
+		}
+
+		if (!isset($data['text']))
+		{
+			if (isset($data['html']))
+			{
+				$data['text'] = $this->converter->convert($data['html']);
+			}
+			else
+			{
+				$this->monolog->error('mail error: mail without body content', ['schema' => $schema]);
+			}
+		}
+
+		if (!$data['to'])
+		{
+			$this->monolog->error('mail error: mail without "to" | subject: ' . $data['subject'], ['schema' => $schema]);
+			return;
+		}
+
+		if (!$data['from'])
+		{
+			$this->monolog->error('mail error: mail without "from" | subject: ' . $data['subject'], ['schema' => $schema]);
+			return;
+		} 
+
+		$message = Swift_Message::newInstance()
+			->setSubject($data['subject'])
+			->setBody($data['text'])
+			->setTo($data['to'])
+			->setFrom($data['from']);
+
+		if (isset($data['html']))
+		{
+			$message->addPart($data['html'], 'text/html');
+		}
+
+		if (isset($data['reply_to']))
+		{
+			$message->setReplyTo($data['reply_to']);
+		}
+
+		if (isset($data['cc']))
+		{
+			$message->setCc($data['cc']);
+		}
+
+		if ($this->mailer->send($message, $failed_recipients))
+		{
+			$this->monolog->info('mail: message send to ' . implode(', ', $data['to']) . ' subject: ' . $data['subject'], ['schema' => $schema]);
+		}
+		else
+		{
+			$this->monolog->error('mail error: failed sending message to ' . implode(', ', $data['to']) . ' subject: ' . $data['subject'], ['schema' => $schema]);
+		}
+
+		if ($failed_recipients)
+		{
+			$this->monolog->error('mail: failed recipients: ' . $failed_recipients, ['schema' => $schema]);
+		}
 	}
 
 	public function queue(array $data)
