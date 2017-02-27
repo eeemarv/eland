16bcf71cc206ec26d3cc6811ca5d10f13713f85d
martti <info@martti.be>
2017-02-27 06:34:12 +0100

    ip in logs ref #82

--- include/default.php
+++ include/default.php
@@ -87,6 +87,8 @@ $app->extend('monolog', function($monolog, $app) {
 			$record['extra']['user_schema'] = $app['s_schema'];
 		}
 
+		$record['extra']['client_ip'] = $_SERVER['REMOTE_ADDR'] ?? '';
+
 		return $record;
 	});
 
