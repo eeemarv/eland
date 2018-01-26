c56abd2549f2d7a685ce716c0deef42295d7cd74
martti <info@martti.be>
2018-01-27 00:24:04 +0100

    fix var

--- queue/geocode.php
+++ queue/geocode.php
@@ -91,7 +91,7 @@ class geocode extends queue_model implements queue_interface
 
 		try
 		{
-			$addresCollection = $geocoder->geocodeQuery(GeocodeQuery::create($adr));
+			$addresCollection = $this->geocoder->geocodeQuery(GeocodeQuery::create($adr));
 
 //			$address_collection = $this->geocoder->geocode($adr);
 
