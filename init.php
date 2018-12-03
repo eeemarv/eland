a38035d677b8e12dbaa012f78930099627c7bdc2
martti <info@martti.be>
2018-12-03 13:31:56 +0100

    queue geocode on init fix #148

--- init.php
+++ init.php
@@ -54,7 +54,9 @@ if ($step == 1)
 	}
 	else
 	{
-		error_log(' -- eLAS/eLAND database needs to upgrade from ' . $currentversion . ' to ' . $schemaversion . ' -- ');
+		error_log(' -- eLAS/eLAND database needs to
+			upgrade from ' . $currentversion .
+			' to ' . $schemaversion . ' -- ');
 
 		while($currentversion < $schemaversion)
 		{
@@ -66,7 +68,9 @@ if ($step == 1)
 			}
 		}
 
-		$m = 'Upgraded database from schema version ' . $dbversion . ' to ' . $currentversion;
+		$m = 'Upgraded database from schema version ' .
+			$dbversion . ' to ' . $currentversion;
+
 		error_log(' -- ' . $m . ' -- ');
 		$app['monolog']->info('DB: ' . $m, ['schema' => $tschema]);
 	}
@@ -112,29 +116,44 @@ else if ($step == 2)
 
 		if (!$found)
 		{
-			$app['db']->update($tschema . '.users', ['"PictureFile"' => null], ['id' => $user_id]);
-			error_log(' -- Profile image not present, deleted in database: ' . $filename . ' -- ');
+			$app['db']->update($tschema . '.users',
+				['"PictureFile"' => null], ['id' => $user_id]);
+
+			error_log(' -- Profile image not present,
+				deleted in database: ' . $filename . ' -- ');
+
 			$app['monolog']->info('cron: Profile image file of user ' .
-				$user_id . ' was not found in bucket: deleted from database. Deleted filename : ' .
+				$user_id . ' was not found in bucket: deleted
+				from database. Deleted filename : ' .
 				$filename, ['schema' => $tschema]);
 		}
 		else if ($f_schema != $tschema)
 		{
-			$new_filename = $tschema . '_u_' . $user_id . '_' . sha1(time() . $filename) . '.jpg';
+			$new_filename = $tschema . '_u_' . $user_id .
+				'_' . sha1(time() . $filename) . '.jpg';
 
 			$err = $app['s3']->img_copy($filename_bucket, $new_filename);
 
 			if ($err)
 			{
 				error_log(' -- error: ' . $err . ' -- ');
-				$app['monolog']->info('init: copy img error: ' . $err, ['schema' => $tschema]);
+
+				$app['monolog']->info('init: copy img error: ' .
+					$err, ['schema' => $tschema]);
+
 				continue;
 			}
 
-			$app['db']->update($tschema . '.users', ['"PictureFile"' => $new_filename], ['id' => $user_id]);
-			error_log(' -- Profile image renamed, old: ' . $filename . ' new: ' . $new_filename . ' -- ');
+			$app['db']->update($tschema . '.users',
+				['"PictureFile"' => $new_filename],
+				['id' => $user_id]);
+
+			error_log(' -- Profile image renamed, old: ' .
+				$filename . ' new: ' . $new_filename . ' -- ');
+
 			$app['monolog']->info('init: Profile image file renamed, Old: ' .
-				$filename . ' New: ' . $new_filename, ['schema' => $tschema]);
+				$filename . ' New: ' . $new_filename,
+				['schema' => $tschema]);
 		}
 	}
 
@@ -189,28 +208,39 @@ else if ($step == 3)
 
 		if (!$found)
 		{
-			$app['db']->delete($tschema . '.msgpictures', ['id' => $id]);
-			error_log(' -- Message image not present, deleted in database: ' . $filename . ' -- ');
+			$app['db']->delete($tschema . '.msgpictures',
+				['id' => $id]);
+
+			error_log(' -- Message image not present,
+				deleted in database: ' . $filename . ' -- ');
+
 			$app['monolog']->info('init: Image file of message ' . $msg_id .
 				' not found in bucket: deleted from database. Deleted : ' .
 				$filename . ' id: ' . $id, ['schema' => $tschema]);
 		}
 		else if ($f_schema != $tschema)
 		{
-			$new_filename = $tschema . '_m_' . $msg_id . '_' . sha1(time() . $filename) . '.jpg';
+			$new_filename = $tschema . '_m_' .
+				$msg_id . '_' . sha1(time() .
+				$filename) . '.jpg';
 
 			$err = $app['s3']->img_copy($filename_bucket, $new_filename);
 
 			if ($err)
 			{
 				error_log(' -- error: ' . $err . ' -- ');
+
 				$app['monolog']->info('init: copy img error: ' . $err,
 					['schema' => $tschema]);
 				continue;
 			}
 
-			$app['db']->update($tschema . '.msgpictures', ['"PictureFile"' => $new_filename], ['id' => $id]);
-			error_log('Profile image renamed, old: ' . $filename . ' new: ' . $new_filename);
+			$app['db']->update($tschema . '.msgpictures',
+				['"PictureFile"' => $new_filename], ['id' => $id]);
+
+			error_log('Profile image renamed, old: ' .
+				$filename . ' new: ' . $new_filename);
+
 			$app['monolog']->info('init: Message image file renamed, Old : ' .
 				$filename . ' New: ' . $new_filename, ['schema' => $tschema]);
 
@@ -229,7 +259,8 @@ else if ($step == 4)
 
 	error_log('*** clear users cache ***');
 
-	$users = $app['db']->fetchAll('select id from ' . $tschema . '.users');
+	$users = $app['db']->fetchAll('select id
+		from ' . $tschema . '.users');
 
 	foreach ($users as $u)
 	{
@@ -241,7 +272,8 @@ else if ($step == 4)
 }
 else if ($step == 5)
 {
-	$app['db']->executeQuery('delete from ' . $tschema . '.tokens');
+	$app['db']->executeQuery('delete from ' .
+		$tschema . '.tokens');
 
 	error_log('*** empty tokens table (is not used anymore) *** ');
 
@@ -250,10 +282,48 @@ else if ($step == 5)
 }
 else if ($step == 6)
 {
-	$app['db']->executeQuery('delete from ' . $tschema . '.city_distance');
+	$app['db']->executeQuery('delete from ' .
+		$tschema . '.city_distance');
 
 	error_log('*** empty city_distance table (is not used anymore) *** ');
-	error_log('** init end **');
 
+	header('Location: ' . $rootpath . 'init.php?step=7');
+	exit;
+}
+else if ($step == 7)
+{
+	error_log('*** Queue for Geocoding, start: ' . $start . ' ***');
+
+	$rs = $app['db']->prepare('select c.id_user, c.value
+		from ' . $tschema . '.contact c, ' .
+			$tschema . '.type_contact tc
+		where c.id_type_contact = tc.id
+			and tc.abbrev = \'adr\'
+		order by c.id_user asc
+		limit 50 offset ' . $start);
+
+	$rs->execute();
+
+	$more_geocoding = false;
+
+	while ($row = $rs->fetch())
+	{
+		$app['queue.geocode']->queue([
+			'adr'		=> $row['value'],
+			'uid'		=> $row['id_user'],
+			'schema'	=> $tschema,
+		]);
+
+		$more_geocoding = true;
+	}
+
+	if ($more_geocoding)
+	{
+		$start += 20;
+		header('Location: ' . $rootpath . 'init.php?step=7&start=' . $start);
+		exit;
+	}
+
+	error_log('*** init finished ***');
 	exit;
 }
