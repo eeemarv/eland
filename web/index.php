6405466c2512600c82096c365ada0d4be033a99c
martti <info@martti.be>
2018-12-14 11:27:32 +0100

    merge master

--- web/index.php
+++ web/index.php
@@ -3,6 +3,8 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../include/default.php';
 
+var_dump('web_index');
+
 $c_locale = $app['controllers_factory'];
 $c_system = $app['controllers_factory'];
 
@@ -10,7 +12,7 @@ $c_system->get('/', function () {
     return 'Blog home page';
 });
 
-$c_locale->mount('/index', $c_system);
+$c_locale->mount('/{system}', $c_system);
 $app->mount('/{_locale}', $c_locale);
 
 $app->run();
