cd19c10e2746bb541c12dc7ef645b846cda45d03
martti <info@martti.be>
2018-12-23 21:17:02 +0100

    account/user twig functions

--- twig/account.php
+++ twig/account.php
@@ -13,9 +13,48 @@ class account
 		$this->user_cache = $user_cache;
 	}
 
-	public function get(int $id, string $schema)
+	public function get(int $id, string $schema):string
 	{
 		$user = $this->user_cache->get($id, $schema);
 		return htmlspecialchars($user['letscode'] . ' ' . $user['name']);
 	}
+
+	public function get_fullname(int $id, string $schema):string
+	{
+		$user = $this->user_cache->get($id, $schema);
+		return htmlspecialchars($user['fullname']);
+	}
+
+	public function get_name(int $id, string $schema):string
+	{
+		$user = $this->user_cache->get($id, $schema);
+		return htmlspecialchars($user['name']);
+	}
+
+	public function get_code(int $id, string $schema):string
+	{
+		$user = $this->user_cache->get($id, $schema);
+		return htmlspecialchars($user['letscode']);
+	}
+
+	public function get_balance(int $id, string $schema):int
+	{
+		$user = $this->user_cache->get($id, $schema);
+		return $user['saldo'];
+	}
+
+	public function get_min(int $id, string $schema):string
+	{
+		$minlimit = $this->user_cache->get($id, $schema)['minlimit'];
+		$minlimit = $minlimit == -999999999 ? '' : $minlimit;
+		return $minlimit;
+	}
+
+	public function get_max(int $id, string $schema):string
+	{
+		$maxlimit = $this->user_cache->get($id, $schema)['maxlimit'];
+		$maxlimit = $maxlimit == 999999999 ? '' : $maxlimit;
+		return $maxlimit;
+	}
+
 }
--- twig/extension.php
+++ twig/extension.php
@@ -24,8 +24,14 @@ class extension extends \Twig_Extension
 			new \Twig_Function('datepicker_format', 'twig\\date_format::datepicker_format'),
 			new \Twig_Function('datepicker_placeholder', 'twig\\date_format::datepicker_placeholder'),
 			new \Twig_Function('config', 'twig\\config::get'),
-			new \Twig_Function('account', 'twig\\account::get'),
 			new \Twig_Function('base_url', 'twig\\base_url::get'),
+			new \Twig_Function('account', 'twig\\account::get'),
+			new \Twig_Function('user_fullname', 'twig\\account::get_fullname'),
+			new \Twig_Function('username', 'twig\\account::get_name'),
+			new \Twig_Function('account_code', 'twig\\account::get_code'),
+			new \Twig_Function('account_balance', 'twig\\account::get_balance'),
+			new \Twig_Function('account_min', 'twig\\account::get_min'),
+			new \Twig_Function('account_max', 'twig\\account::get_max'),
 		];
 	}
 
