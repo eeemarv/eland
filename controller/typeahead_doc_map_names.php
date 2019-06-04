c17625a435c3680ca531a1d593086bb67a705f69
martti <info@martti.be>
2019-06-04 23:34:49 +0200

    controller typeahead_doc_map_names

---
+++ controller/typeahead_doc_map_names.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace controller;
+
+use util\app;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class typeahead_doc_map_names
+{
+    public function get(app $app):Response
+    {
+        $map_names = [];
+
+        $st = $app['db']->prepare('select distinct data->>\'map_name\' as map_name
+            from xdb.aggs
+            where agg_type = \'doc\'
+                and agg_schema = ?
+                and data->>\'map_name\' <> \'\'
+            order by data->>\'map_name\' asc');
+
+        $st->bindValue(1, $app['tschema']);
+
+        $st->execute();
+
+        while ($row = $st->fetch())
+        {
+            $map_names[] = $row['map_name'];
+        }
+
+        $params = [
+            'schema'	=> $app['tschema'],
+        ];
+
+        $crc = crc32(json_encode($map_names));
+
+        $app['typeahead']->set_thumbprint('doc_map_names', $params, $crc);
+
+        return $app->json($map_names);
+    }
+}
