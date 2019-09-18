51aeaf660294f0d5744070b352883417eaa090b4
martti <info@martti.be>
2019-09-18 14:07:56 +0200

    route home_system

--- controller/home_system.php
+++ controller/home_system.php
@@ -3,13 +3,23 @@
 namespace controller;
 
 use util\app;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class home_system
 {
     public function home_system(app $app):Response
     {
-        return $app['legacy_route']->render('home_system');
+        $out = '<div class="jumbotron">';
+        $out .= '<h1>';
+        $out .= $app['config']->get('systemname', $app['pp_schema']);
+        $out .= '</h1>';
+        $out .= '</div>';
+
+        $app['menu']->set('home_system');
+
+        return $app->render('base/sidebar.html.twig', [
+            'content'   => $out,
+            'schema'    => $app['pp_schema'],
+        ]);
     }
 }
--- web/index.php
+++ web/index.php
@@ -1,5 +1,15 @@
 <?php declare(strict_types=1);
 
+if (substr_count($_SERVER['SERVER_NAME'], '.') === 2)
+{
+    require_once __DIR__ . '/../include/web_legacy.php';
+
+    header('Location: ' . $app->url('home_system', [
+        'system'		=> $system,
+    ]));
+    exit;
+}
+
 require_once __DIR__ . '/../include/app.php';
 
 $app->run();
