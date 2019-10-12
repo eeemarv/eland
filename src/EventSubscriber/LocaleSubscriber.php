bd733ec4c26bfc7438297f70b935392410d98ac8
martti <info@martti.be>
2019-10-12 10:21:14 +0200

    locale subscriber

---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    const LOCALES = [
+        'nl'    => 'nl_NL.UTF-8',
+        'en'    => 'en_GB.UTF-8',
+    ];
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $locale = $event->getRequest()->getLocale();
+
+        if (isset(self::LOCALES[$locale]))
+        {
+            setlocale(LC_TIME, self::LOCALES[$locale]);
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+           'kernel.request' => 'onKernelRequest',
+        ];
+    }
+}
