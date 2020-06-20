ab5f77e6134aff28be8240a19dbf6ba09fff79fa
martti <info@martti.be>
2020-06-20 15:10:17 +0200

    admin menu and config_name tpl

---
+++ src/Command/Config/ConfigNameCommand.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace App\Command\Config;
+
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Sequentially;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+
+class ConfigNameCommand
+{
+    public $system_name;
+    public $email_tag;
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraint('system_name', new Sequentially([
+            'constraints'   => [
+                new NotBlank(),
+                new Length(['max' => 60]),
+            ],
+        ]));
+        $metadata->addPropertyConstraint('email_tag', new Sequentially([
+            'constraints'   => [
+                new NotBlank(),
+                new Length(['max' => 20]),
+            ],
+        ]));
+    }
+}
