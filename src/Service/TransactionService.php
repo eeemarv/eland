dda681b7795474ac34d47e43da02b899524a721c
martti <info@martti.be>
2020-07-19 20:28:09 +0200

    transactions_add update balance

--- src/Service/TransactionService.php
+++ src/Service/TransactionService.php
@@ -4,7 +4,6 @@ namespace App\Service;
 
 use Doctrine\DBAL\Connection as Db;
 use Psr\Log\LoggerInterface;
-use App\Service\UserCacheService;
 use App\Service\AutoMinLimitService;
 use App\Service\ConfigService;
 use App\Render\AccountRender;
@@ -13,7 +12,6 @@ class TransactionService
 {
 	protected Db $db;
 	protected LoggerInterface $logger;
-	protected UserCacheService $user_cache_service;
 	protected AutoMinLimitService $autominlimit_service;
 	protected ConfigService $config_service;
 	protected AccountRender $account_render;
@@ -21,7 +19,6 @@ class TransactionService
 	public function __construct(
 		Db $db,
 		LoggerInterface $logger,
-		UserCacheService $user_cache_service,
 		AutoMinLimitService $autominlimit_service,
 		ConfigService $config_service,
 		AccountRender $account_render
@@ -29,7 +26,6 @@ class TransactionService
 	{
 		$this->db = $db;
 		$this->logger = $logger;
-		$this->user_cache_service = $user_cache_service;
 		$this->autominlimit_service = $autominlimit_service;
 		$this->config_service = $config_service;
 		$this->account_render = $account_render;
@@ -46,39 +42,41 @@ class TransactionService
 
 	public function insert(array $transaction, string $schema):int
 	{
+		$from_id = (int) $transaction['id_from'];
+		$to_id = (int) $transaction['id_to'];
+		$amount = (int) $transaction['amount'];
+
 		$this->db->beginTransaction();
 
-		try
-		{
-			$this->db->insert($schema . '.transactions', $transaction);
-			$id = (int) $this->db->lastInsertId($schema . '.transactions_id_seq');
-			$this->db->executeUpdate('update ' . $schema . '.users set balance = balance + ? where id = ?', [$transaction['amount'], $transaction['id_to']]);
-			$this->db->executeUpdate('update ' . $schema . '.users set balance = balance - ? where id = ?', [$transaction['amount'], $transaction['id_from']]);
-			$this->db->commit();
+		$this->db->insert($schema . '.transactions', $transaction);
+		$id = (int) $this->db->lastInsertId($schema . '.transactions_id_seq');
 
-		}
-		catch(\Exception $e)
-		{
-			$this->db->rollback();
-			throw $e;
-			return 0;
-		}
+		$this->db->executeUpdate('insert into ' . $schema . '.balance (account_id, amount, balance)
+			values (?, ?, (select coalesce(balance, 0) + ? from ' . $schema . '.balance
+			where account_id = ?
+			order by id desc limit 1))',
+			[$to_id, $amount, $amount, $to_id],
+			[\PDO::PARAM_INT, \PDO::PARAM_INT, \PDO::PARAM_INT, \PDO::PARAM_INT]);
 
-		$this->user_cache_service->clear($transaction['id_to'], $schema);
-		$this->user_cache_service->clear($transaction['id_from'], $schema);
+		$this->db->executeUpdate('insert into ' . $schema . '.balance (account_id, amount, balance)
+			values (?, 0 - ?, (select coalesce(balance, 0) - ? from ' . $schema . '.balance
+			where account_id = ?
+			order by id desc limit 1))',
+			[$from_id, $amount, $amount, $from_id],
+			[\PDO::PARAM_INT, \PDO::PARAM_INT, \PDO::PARAM_INT, \PDO::PARAM_INT]);
+
+		$this->db->commit();
 
 		$this->autominlimit_service->init($schema)
-			->process($transaction['id_from'],
-				$transaction['id_to'],
-				(int) $transaction['amount']);
+			->process($from_id, $to_id, $amount);
 
 		$this->logger->info('Transaction ' . $transaction['transid'] . ' saved: ' .
-			$transaction['amount'] . ' ' .
+			$amount . ' ' .
 			$this->config_service->get('currency', $schema) .
 			' from user ' .
-			$this->account_render->str_id($transaction['id_from'], $schema) .
+			$this->account_render->str_id($from_id, $schema) .
 			' to user ' .
-			$this->account_render->str_id($transaction['id_to'], $schema),
+			$this->account_render->str_id($to_id, $schema),
 			['schema' => $schema]);
 
 		return $id;
