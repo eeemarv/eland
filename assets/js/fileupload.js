07048f34d3c812db677e6ae8dff6d3f4639e0de6
martti <info@martti.be>
2020-08-10 09:31:04 +0200

    temp

--- assets/js/fileupload.js
+++ assets/js/fileupload.js
@@ -3,14 +3,18 @@ import 'blueimp-file-upload/css/jquery.fileupload.css';
 import 'blueimp-file-upload/js/vendor/jquery.ui.widget';
 import 'blueimp-file-upload/js/jquery.iframe-transport';
 
+import 'blueimp-load-image';
+/*
 import 'blueimp-load-image/js/load-image-exif';
 import 'blueimp-load-image/js/load-image-orientation';
 import 'blueimp-load-image/js/load-image-scale';
 import 'blueimp-load-image/js/load-image-meta';
 import 'blueimp-load-image/js/load-image-fetch';
 import 'blueimp-load-image/js/load-image-iptc';
+*/
 
-import 'blueimp-canvas-to-blob/js/canvas-to-blob';
+import 'blueimp-canvas-to-blob';
+//import 'blueimp-canvas-to-blob/js/canvas-to-blob';
 
 import 'blueimp-file-upload/js/jquery.fileupload';
 
