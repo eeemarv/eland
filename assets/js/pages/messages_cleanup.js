1a113e6fdfcd6febb0a8f6e2d22ac12d12a4f0ef
martti <info@martti.be>
2020-08-30 12:08:02 +0200

    disable prop on after_days

--- assets/js/pages/messages_cleanup.js
+++ assets/js/pages/messages_cleanup.js
@@ -1,20 +1,37 @@
 export default function() {
-	var $input_switch = $('[data-expires-at-switch-enabled]');
+	var $input_switch_enabled = $('[data-expires-at-switch-enabled]');
 	var $input_days_default = $('[data-expires-at-days-default]');
 	var $input_required = $('[data-expires-at-required]');
-	function set()
+	var $input_cleanup_enabled = $('[data-cleanup-enabled]');
+	var $input_cleanup_after_days = $('[data-cleanup-after-days]');
+	function set_switch_enabled_prop()
 	{
 		if (!$input_days_default.val() || $input_required.prop('checked')){
-			$input_switch.prop('disabled', true);
+			$input_switch_enabled.prop('disabled', true);
 			return;
 		}
-		$input_switch.prop('disabled', false);
+		$input_switch_enabled.prop('disabled', false);
 	}
-	set();
+	function set_cleanup_after_days_prop()
+	{
+		if (!$input_cleanup_enabled.prop('checked')){
+			$input_cleanup_after_days.prop('disabled', true);
+			return;
+		}
+		$input_cleanup_after_days.prop('disabled', false);
+	}
+	set_switch_enabled_prop();
+	set_cleanup_after_days_prop();
 	$input_days_default.on('keyup keydown change', function(){
-		set();
+		set_switch_enabled_prop();
 	});
 	$input_required.on('change', function(){
-		set();
+		set_switch_enabled_prop();
+	});
+	$input_cleanup_enabled.on('change', function(){
+		set_cleanup_after_days_prop();
+	});
+	$input_cleanup_after_days.closest('form').submit(function(){
+		$input_cleanup_after_days.prop('disabled', false);
 	});
 };
