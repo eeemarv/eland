c0b8eb7ee796651d6b8c8a79384c6e499195fb18
martti <info@martti.be>
2020-09-16 08:21:52 +0200

    guest voter

--- src/Security/Voter/ElandRoleVoter.php
+++ src/Security/Voter/ElandRoleVoter.php
@@ -5,6 +5,7 @@ namespace App\Security\Voter;
 use App\Cnst\AccessCnst;
 use App\Cnst\RoleCnst;
 use App\Service\ConfigService;
+use App\Service\IntersystemsService;
 use App\Service\PageParamsService;
 use App\Service\SessionUserService;
 use App\Service\SystemsService;
@@ -16,23 +17,23 @@ class ElandRoleVoter extends Voter
 {
     protected SessionUserService $su;
     protected PageParamsService $pp;
-    protected RequestStack $request_stack;
-    protected string $system;
-    protected string $org_system;
     protected ConfigService $config_service;
     protected SystemsService $systems_service;
+    protected IntersystemsService $intersystems_service;
 
     public function __construct(
         SessionUserService $su,
         PageParamsService $pp,
         ConfigService $config_service,
-        SystemsService $systems_service
+        SystemsService $systems_service,
+        IntersystemsService $intersystems_service
     )
     {
         $this->su = $su;
         $this->pp = $pp;
         $this->config_service = $config_service;
         $this->systems_service = $systems_service;
+        $this->intersystems_service = $intersystems_service;
     }
 
     protected function supports($attribute, $subject):bool
@@ -61,10 +62,24 @@ class ElandRoleVoter extends Voter
                 return false;
             }
 
-            if ($attribute === 'guest'
-                && !$this->config_service->get_intersystem_en($schema))
+            if ($attribute === 'guest')
             {
-                return false;
+                if (!$this->config_service->get_intersystem_en($schema))
+                {
+                    return false;
+                }
+
+                $org_schema = $this->pp->org_schema();
+
+                if ($org_schema !== '')
+                {
+                    $eland_ary = $this->intersystems_service->get_eland($org_schema);
+
+                    if (!isset($eland_ary[$schema]))
+                    {
+                        return false;
+                    }
+                }
             }
         }
 
