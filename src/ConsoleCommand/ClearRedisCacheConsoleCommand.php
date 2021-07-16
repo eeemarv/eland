f453b8d0705f921b7570c2d5d011daf41d328d66
martti <info@martti.be>
2021-07-16 18:58:53 +0200

    rename console commands & cms_edit include

--- src/ConsoleCommand/ClearRedisCacheConsoleCommand.php
+++ src/ConsoleCommand/ClearRedisCacheConsoleCommand.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ClearRedisCacheCommand extends Command
+class ClearRedisCacheConsoleCommand extends Command
 {
     protected static $defaultName = 'app:clear-redis-cache';
 
