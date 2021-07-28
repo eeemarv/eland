d6ec1f7ec3b0caeef7da874f1327c5fecc26f878
martti <info@martti.be>
2021-07-28 23:24:41 +0200

    logic for forum_del_post tpl

--- src/Controller/Forum/ForumDelPostController.php
+++ src/Controller/Forum/ForumDelPostController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller\Forum;
 
+use App\Form\Post\DelType;
+use App\Render\AccountRender;
 use App\Render\LinkRender;
+use App\Repository\ForumRepository;
 use App\Service\AlertService;
 use App\Service\ConfigService;
-use App\Service\FormTokenService;
 use App\Service\ItemAccessService;
 use App\Service\PageParamsService;
 use App\Service\SessionUserService;
-use Doctrine\DBAL\Connection as Db;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -36,9 +37,9 @@ class ForumDelPostController extends AbstractController
     public function __invoke(
         Request $request,
         int $id,
-        Db $db,
+        ForumRepository $forum_repository,
         LinkRender $link_render,
-        FormTokenService $form_token_service,
+        AccountRender $account_render,
         ConfigService $config_service,
         AlertService $alert_service,
         PageParamsService $pp,
@@ -51,81 +52,49 @@ class ForumDelPostController extends AbstractController
             throw new NotFoundHttpException('Forum module not enabled.');
         }
 
-        $forum_post = $db->fetchAssociative('select *
-            from ' . $pp->schema() . '.forum_posts
-            where id = ?', [$id], [\PDO::PARAM_INT]);
+        $forum_post = $forum_repository->get_post($id, $pp->schema());
+        $forum_topic = $forum_repository->get_topic($forum_post['topic_id'], $pp->schema());
 
-        if (!isset($forum_post) || !$forum_post)
+        if (!$item_access_service->is_visible($forum_topic['access']))
         {
-            throw new NotFoundHttpException('Forum post niet gevonden.');
+            throw new AccessDeniedHttpException('Access denied for forum topic ' . $id . '.');
         }
 
-        $s_post_owner = $su->id() === $forum_post['user_id']
-            && $su->is_system_self() && !$pp->is_guest();
-
-        if (!($pp->is_admin() || $s_post_owner))
+        if (!($su->is_owner($forum_post['user_id']) || $pp->is_admin()))
         {
-            throw new AccessDeniedHttpException('Je hebt onvoldoende rechten om deze reactie te verwijderen.');
+            throw new AccessDeniedHttpException('No rights for this action.');
         }
 
-        $forum_topic = ForumTopicController::get_forum_topic($forum_post['topic_id'], $db, $pp, $item_access_service);
-
-        $first_post_id = $db->fetchOne('select id
-            from ' . $pp->schema() . '.forum_posts
-            where topic_id = ?
-            order by created_at asc
-            limit 1',
-            [$forum_topic['id']], [\PDO::PARAM_INT]);
+        $first_post_id = $forum_repository->get_first_post_id($forum_topic['id'], $pp->schema());
 
         if ($first_post_id === $id)
         {
-            throw new AccessDeniedHttpException('Verkeerde route om eerste post aan te verwijderen');
+            throw new AccessDeniedHttpException('Wrong route for this action.');
         }
 
-        if ($request->isMethod('POST'))
+        $form = $this->createForm(DelType::class)
+            ->handleRequest($request);
+
+        if ($form->isSubmitted()
+            && $form->isValid())
         {
-            $errors = [];
-
-            if ($error_token = $form_token_service->get_error())
+            if ($forum_repository->del_post($id, $pp->schema()))
             {
-                $errors[] = $error_token;
+                $topic_subject = $forum_topic['subject'];
+                $account_str = $account_render->str($forum_post['user_id'], $pp->schema());
+
+                $alert_msg = $su->is_owner($forum_post['user_id']) ? 'Je post' : 'De post van ' . $account_str;
+                $alert_msg .= ' in topic "' . $topic_subject . '" werd gewist.';
+
+                $alert_service->success($alert_msg);
+                $link_render->redirect('forum_topic', $pp->ary(), ['id' => $forum_topic['id']]);
             }
 
-            if (!count($errors))
-            {
-                $db->delete($pp->schema() . '.forum_posts', ['id' => $id]);
-
-                $alert_service->success('De forum reactie is verwijderd.');
-
-                $link_render->redirect('forum_topic', $pp->ary(),
-                    ['id' => $forum_post['topic_id']]);
-            }
+            $alert_service->error('Fout bij verwijderen van post');
         }
-
-        $out = '<div class="panel panel-info">';
-        $out .= '<div class="panel-heading">';
-
-        $out .= '<p>';
-        $out .= $forum_post['content'];
-        $out .= '</p>';
-
-        $out .= '<form method="post">';
-
-        $out .= $link_render->btn_cancel('forum_topic', $pp->ary(),
-            ['id' => $forum_post['topic_id']]);
-
-        $out .= '&nbsp;';
-        $out .= '<input type="submit" value="Verwijderen" ';
-        $out .= 'name="zend" class="btn btn-danger btn-lg">';
-        $out .= $form_token_service->get_hidden_input();
-
-        $out .= '</form>';
-
-        $out .= '</div>';
-        $out .= '</div>';
 
         return $this->render('forum/forum_del_post.html.twig', [
-            'content'       => $out,
+            'form'          => $form->createView(),
             'forum_topic'   => $forum_topic,
             'forum_post'    => $forum_post,
         ]);
