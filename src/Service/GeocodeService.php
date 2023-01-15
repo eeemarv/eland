53dbdd2b2e7d69864820813b462f963b545aa794
martti <info@martti.be>
2023-01-15 22:44:16 +0100

    parse address for bpost

--- src/Service/GeocodeService.php
+++ src/Service/GeocodeService.php
@@ -17,16 +17,86 @@ class GeocodeService
 
     public function getCoordinates(string $address):array
     {
+        $comma_count = substr_count($address, ',');
+
+        if ($comma_count !== 1){
+            error_log('Geocode adr: ' . $address . ' malformation comma-count');
+            return [];
+        }
+
+        [$street, $loc] = explode(',', $address);
+
+        $street_parts = explode(' ', $street);
+        $loc_parts = explode(' ', $loc);
+
+        $postal_code = '';
+        $locality = '';
+
+        foreach($loc_parts as $lp)
+        {
+            if ($postal_code !== '')
+            {
+                $postal_code += $lp;
+                continue;
+            }
+            if ($locality !== '')
+            {
+                $locality += ' ';
+            }
+            $locality += $lp;
+        }
+
+        $street_name = '';
+        $street_number = '';
+        $street_bus = '';
+
+        foreach ($street_parts as $st)
+        {
+            if ($street_name !== '')
+            {
+                if ($street_number === '')
+                {
+                    if (ctype_digit(substr($st, 0, 1)))
+                    {
+                        $street_number = $st;
+                        continue;
+                    }
+                }
+                else
+                {
+                    if ($street_bus !== '')
+                    {
+                        $street_bus += ' ';
+                    }
+                    $street_bus += $st;
+                    continue;
+                }
+                $street_name += ' ';
+            }
+
+            $street_name += $st;
+        }
+
+        error_log('-- GEOCDDE parsed address ---');
+        error_log('street_nmae: ' . $street_name);
+        error_log('street_number: ' . $street_number);
+        error_log('street_bus: ' . $street_bus);
+        error_log('postal_code' . $postal_code);
+        error_log('locality' . $locality);
+        error_log('-- -- -- -- -- -- -- --');
+
         $provider = new bpost($this->client_interface);
         $geocoder = new StatefulGeocoder($provider, 'nl');
         $geocoder->setLimit(1);
 
         try
         {
-            $f_address = strtoupper($address);
-            $f_address = str_replace(',', ' -', $f_address);
-
-            $addressCollection = $geocoder->geocodeQuery(GeocodeQuery::create($f_address));
+            $query = GeocodeQuery::create($address)
+                ->withData('streetNumber', $street_number)
+                ->withData('streetName', $street_name)
+                ->withData('postalCode', $postal_code)
+                ->withData('locality', $locality);
+            $addressCollection = $geocoder->geocodeQuery($query);
 
             if (is_object($addressCollection))
             {
